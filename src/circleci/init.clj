8cfe31bba74eb8d81a414054d0ffe9a42174d6b6
Allen Rohner <arohner@gmail.com>
2011-09-06 10:56:27 -0500

    Disable swank

--- src/circleci/init.clj
+++ src/circleci/init.clj
@@ -1,12 +1,12 @@
 (ns circleci.init
-  (:require circleci.swank)
+  ;;(:require circleci.swank)
   (:require circleci.db)
   (:require circleci.db.migrations)
   (:require circleci.web)
   (:require circleci.repl))
 
 (defn -main []
-  (circleci.swank/init)
+  ;; (circleci.swank/init)
   (circleci.db/init)
   (circleci.db.migrations/init)
   (circleci.web/init)
