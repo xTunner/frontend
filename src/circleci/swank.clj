c6ba527e7ccc1b8ba4a1d6d02c18a680c0d5293b
Allen Rohner <arohner@gmail.com>
2011-09-07 16:22:20 -0500

    Try restoring swank

--- src/circleci/swank.clj
+++ src/circleci/swank.clj
@@ -1,10 +1,10 @@
-;; (ns circleci.swank
-;;   (:require swank.swank))
+(ns circleci.swank
+  (:require swank.swank))
 
-;; (defn init []
-;;   (binding [*print-length* 100
-;;             *print-level* 20]
-;;   (println "Starting Swank")
-;;   (clojure.main/with-bindings
-;;     (swank.swank/start-server :port 4005
-;;                               :encoding "utf-8"))))
+(defn init []
+  (binding [*print-length* 100
+            *print-level* 20]
+  (println "Starting Swank")
+  (clojure.main/with-bindings
+    (swank.swank/start-server :port 4005
+                              :encoding "utf-8"))))
