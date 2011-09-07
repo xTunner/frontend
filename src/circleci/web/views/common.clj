a3a180b60d8bcbacb35ac4e02e93d1933c90e000
Allen Rohner <arohner@gmail.com>
2011-09-07 16:42:25 -0500

    Add google analytics

--- src/circleci/web/views/common.clj
+++ src/circleci/web/views/common.clj
@@ -6,9 +6,23 @@
 (defn css [path & {:keys [rel type media title] :as opts}]
   [:link (merge {:href path} opts)])
 
+(defn google-analytics []
+  (html
+   [:script {:type "text/javascript"} "
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25580673-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();"]))
+
 (defpartial layout [& content]
             (html5
              [:head
+              (google-analytics)
               [:meta {:name "google-site-verification" :content "rCckS33lTuN6tiIrxLMykv_uRR0dMoHNM4XrR4yYUQ0"}]
               [:meta {:http-equiv "Content-Type"
                       :content "text/html; charset=utf-8"}]
