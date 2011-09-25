6f1154e7fb282f99e3d09da1011fd9a8dcef0a2a
Allen Rohner <arohner@gmail.com>
2011-09-25 13:57:11 -0500

    Change the rvm dep

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject circleci "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.2.1"]
+                           [clj-json "0.4.0"] ;; noir pulls in clj-json 0.3.2 which isn't compatible w/ clojure 1.3. Put this dep ahead to pull it in first.
                            [noir "1.1.1-SNAPSHOT"]
                            [clj-table "0.1.5"]
                            [clj-url "1.0.2"]
@@ -9,8 +10,12 @@
                            [log4j "1.2.14"]
                            [log4j/apache-log4j-extras "1.1"]
                            [commons-codec "1.4"]
+                           [arohner-utils "0.0.2"]
+                           [org.clojars.arohner/clj-yaml "fc59d5c4337614734bba24ea2ee90aa5237dde5b"]
+                           [org.danlarkin/clojure-json "1.2-SNAPSHOT"]
                            [org.cloudhoist/pallet "0.6.4"]
                            [org.cloudhoist/pallet-crates-all "0.5.1-SNAPSHOT"]
+                           [pallet-rvm "0.1"]
                            [org.jclouds/jclouds-all "1.0.0"]
                            [org.jclouds.driver/jclouds-log4j "1.0.0"]
                            [org.jclouds.driver/jclouds-jsch "1.0.0"]]
