52816272fdbb81547acdd9b68c6d5f0a0ed4dc05
Paul Biggar <paul.biggar@gmail.com>
2011-09-27 17:19:00 -0700

    reorder feature boxes.

--- src/circleci/web/views/index.clj
+++ src/circleci/web/views/index.clj
@@ -19,11 +19,11 @@
     [:div#content
      [:div#main_content_wide.left
       [:div.box_medium.feature.separator_r
-       [:img {:src "img/icon_feature_01.png"
+       [:img {:src "img/icon_feature_03.png"
               :width 60
               :height 55}]
-       [:h3 "Easy Github Integration"]
-       [:p "Automatically run the tests after every commit"]]
+       [:h3 "No more build breaks"]
+       [:p "Circle runs automated tests, builds artifacts, manages integration branches and deploys to production, with ease" ]]
       [:div.box_medium.feature
        [:img {:src "img/icon_feature_02.png"
               :width 60
@@ -31,11 +31,11 @@
        [:h3 "Staged Deployments"]
        [:p "Control when and how to deploy to automatically production, after the tests pass"]]
       [:div.box_medium.feature.separator_r
-       [:img {:src "img/icon_feature_03.png"
+       [:img {:src "img/icon_feature_01.png"
               :width 60
               :height 55}]
-       [:h3 "No more build breaks"]
-       [:p "CircleCI runs automated tests, builds artifacts, manages integration branches and deploys to production, with ease" ]]
+       [:h3 "Easy Github Integration"]
+       [:p "Automatically run the tests after every commit"]]
       [:div.box_medium.feature
        [:img {:src "img/icon_feature_04.png"
               :width 60
