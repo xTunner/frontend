9df75e9799794ec03997895900f7be4b05430b08
Paul Biggar <paul.biggar@gmail.com>
2011-09-29 12:16:09 -0700

    Remove the wufoo form, it work's well without it.

--- src/circleci/web/views/index.clj
+++ src/circleci/web/views/index.clj
@@ -22,77 +22,15 @@
       [:h1#cititle "Continuous Integration" [:br] "made easy"]]
 
      [:div#ciright
-      [:div#signupform
-      "<form id=\"form3\" name=\"form3\" class=\"wufoo topLabel page1\" autocomplete=\"off\" enctype=\"multipart/form-data\" method=\"post\" novalidate=\"\" action=\"http://paulandorla.wufoo.com/forms/interested/#public\">
-
-        <header id=\"header\" class=\"info\">
-        <h2>Interested?</h2>
-        <div>Get notified when our beta is ready.</div>
-        </header>
-
-        <ul>
-
-          <li id=\"fo3li101\" class=\"     \">
-          <label class=\"desc\" id=\"title101\" for=\"Field101\">
-            Email
-            <span id=\"req_101\" class=\"req\">*</span>
-          </label>
-          <div>
-            <input id=\"Field101\" name=\"Field101\" spellcheck=\"false\" class=\"field text medium\" maxlength=\"255\" tabindex=\"1\" onkeyup=\"handleInput(this);\" onchange=\"handleInput(this);\" required=\"\" type=\"email\"> 
-          </div>
-          </li>
-
-
-
-          <li id=\"fo3li1\" class=\"     \">
-          <fieldset>
-            <!--[if !IE | (gte IE 8)]-->
-            <legend id=\"title1\" class=\"desc\">
-            </legend>
-            <!--[endif]-->
-            <!--[if lt IE 8]>
-            <label id=\"title1\" class=\"desc\">
-            </label>
-            <![endif]-->
-            <div>
-              <span>
-                <input id=\"Field1\" name=\"Field1\" class=\"field checkbox\" value=\"May we contact you to ask about your platform/stack/test suite/etc?\" tabindex=\"2\" onchange=\"handleInput(this);\" type=\"checkbox\">
-                <label class=\"choice\" for=\"Field1\">May we contact you to ask about your platform/stack/test suite/etc?</label>
-              </span>
-            </div>
-          </fieldset>
-          </li>
-
-
-          <li class=\"buttons \">
-          <div>
-            <input name=\"currentPage\" id=\"currentPage\" value=\"BbVQyKfGqbsY12gqY0UqbQ6kGoN9nWBKExYDGjFVwuBeNQ=\" type=\"hidden\">
-
-            <input tabindex=\"21\" id=\"saveForm\" name=\"saveForm\" class=\"btTxt submit\" value=\"Submit\" onmousedown=\"doSubmitEvents();\" type=\"submit\">
-          </div>
-          </li>
-        </ul>
-      </form> 
-    </div><!--container-->
-    <img id=\"bottom\" src=\"img/bottom.png\" alt=\"\">
-
+      [:div
+      "
     <style type=\"text/css\">
       @import url(/css/global/power.15346.css);
     </style>
 
     <!-- JavaScript -->
     <script src=\"js/dynamic.js\"></script>
-
-    <script type=\"text/javascript\">
-      __RULES = [];
-      __ENTRY = [];
-      __PRICES = null;
-    </script>"
-
-
-
-
-
+    "
 
       [:h3#takepart "Take part in the beta"]
       [:p
