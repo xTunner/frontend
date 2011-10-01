df97b3f9ca4c2558faa7ce50d98d71d562287e1a
Paul Biggar <paul.biggar@gmail.com>
2011-10-01 15:39:46 -0700

    Improve copy.

--- src/circleci/web/views/index.clj
+++ src/circleci/web/views/index.clj
@@ -48,31 +48,28 @@
     [:div#content_wrap
       [:div#content
         [:div#main_content_wide.left
+
           [:div.box_medium.feature.separator_r
-            [:img {:src "img/icon_feature_03.png"
-                   :width 60
-                   :height 55}]
-            [:h3 "No more build breaks"]
-            [:p "Circle runs automated tests, builds artifacts, manages integration branches "
-                "and deploys to production, with ease" ]]
+            [:img {:src "img/icon_feature_04.png" :width 60 :height 55}]
+            [:h3 "Minimal configuration"]
+            [:p "In most cases, we'll just need access to your repo."]]
+
           [:div.box_medium.feature
-            [:img {:src "img/icon_feature_02.png"
-                   :width 60
-                   :height 55}]
-            [:h3 "Staged Deployments"]
-            [:p "Control when and how to deploy to automatically production, after the tests pass"]]
+            [:img {:src "img/icon_feature_01.png" :width 60 :height 55}]
+            [:h3 "Go home early"]
+            [:p "Push to a branch, and we'll automatically merge once the tests pass."]]
+
           [:div.box_medium.feature.separator_r
-            [:img {:src "img/icon_feature_01.png"
-                   :width 60
-                   :height 55}]
-            [:h3 "Easy Github Integration"]
-            [:p "Automatically run the tests after every commit"]]
+            [:img {:src "img/icon_feature_02.png" :width 60 :height 55}]
+            [:h3 "Lightning fast tests"]
+            [:p "Find out as soon as something breaks, using automatic paralleization, "
+                "dependency analysis and test prioritization."]]
+
           [:div.box_medium.feature
-            [:img {:src "img/icon_feature_04.png"
-                   :width 60
-                   :height 55}]
-            [:h3 "Parallel testing"]
-            [:p "Reduce test time by running the tests in parallel on multiple boxes"]]]]]))
+            [:img {:src "img/icon_feature_03.png" :width 60 :height 55}]
+            [:h3 "Continuous deployment"]
+            [:p "Deploy your web app to prodution automatically once tests pass. "
+                "Revert automatically based on your analytics data."]]]]]))
 
 (defpartial signupform [& content]
    [:div.move-right
