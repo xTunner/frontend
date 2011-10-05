637caef287545f36b8c09dd332e4bce7c53f31a7
Allen Rohner <arohner@gmail.com>
2011-10-05 10:49:58 -0500

    Start daemon correctly

--- src/circleci/backend/project/circleci.clj
+++ src/circleci/backend/project/circleci.clj
@@ -20,6 +20,6 @@
                           :group circle/circleci-group
                           :actions [(checkout "git@github.com:arohner/CircleCI.git")
                                     (bash [(lein deps)
-                                           (lein daemon start :web)])
+                                           (lein daemon start ":web")])
                                     ;;; Load Balancer
                                     ]))
