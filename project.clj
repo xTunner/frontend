2d34761603da8332978843272e7535985b9b2e1c
Allen Rohner <arohner@gmail.com>
2011-10-06 11:56:06 -0500

    Bump the jclouds versions again, actually fixes the AWS firewall issue

--- project.clj
+++ project.clj
@@ -19,13 +19,13 @@
                  [com.amazonaws/aws-java-sdk "1.2.7"]
                  
                  [org.cloudhoist/pallet "0.6.5-SNAPSHOT"]
-                 [org.jclouds/jclouds-core "1.1.1"]
-                 [org.jclouds/jclouds-compute "1.1.1"]
-                 [org.jclouds/jclouds-blobstore "1.1.1"]
-                 [org.jclouds/jclouds-loadbalancer "1.1.1"]
-                 [org.jclouds.driver/jclouds-log4j "1.1.1"]
-                 [org.jclouds.driver/jclouds-jsch "1.1.1"]
-                 [org.jclouds.provider/aws-ec2 "1.1.1"]
+                 [org.jclouds/jclouds-core "1.2.0-20111005.184141-97"]
+                 [org.jclouds/jclouds-compute "1.2.0-20111005.184527-96"]
+                 [org.jclouds/jclouds-blobstore "1.2.0-20111005.185008-93"]
+                 [org.jclouds/jclouds-loadbalancer "1.2.0-20111005.184815-94"]
+                 [org.jclouds.driver/jclouds-log4j "1.2.0-20111005.184406-97"]
+                 [org.jclouds.driver/jclouds-jsch "1.2.0-20111005.191220-90"]
+                 [org.jclouds.provider/aws-ec2 "1.2.0-20111005.190523-90"]
 
                  ;; Pallet Crates
                  [org.cloudhoist/automated-admin-user "0.6.0"]
