6a4db0b81a66d8d94ac30679f10611e8eace2341
Paul Biggar <paul.biggar@gmail.com>
2011-10-07 00:40:01 -0700

    Add simple pattern matching. Matchure and more complex matching weren't cutting it.

--- src/circleci/backend/proto/src/proto/main.clj
+++ src/circleci/backend/proto/src/proto/main.clj
@@ -1,7 +1,33 @@
 (ns proto.main
   (:require [clojure.contrib.io :as io])
-  (:require [clj-yaml.core :as yaml])
-  )
+  (:require [clojure.contrib.generic.functor :as functor])
+  (:require [clj-yaml.core :as yaml]))
+
+(declare autotools-handler)
+
+(def handlers '(autotools-handler))
+
+
+(defn run-handlers
+  "Run all the handlers on a definition"
+  [config]
+  (map #(% config) handlers))
+
+
+(defn process-handlers
+  "For each project definition, run all the handlers on it"
+  [configurations]
+  (functor/fmap run-handlers configurations))
+
+
+(defn autotools-handler
+  [{type :type
+    subdir :subdir
+    configurations :configurations
+    autoconf-version :autoconf-version}]
+  (when (= type "autotools") "print autotools"))
+
+
 
 (defn init [& argv]
   (-> argv
@@ -9,7 +35,7 @@
       io/reader
       slurp
       yaml/parse-string
-      println))
+      process-handlers))
 
 
 (defn -main [& argv]
