63ad1613057ffd629739f60bd036d403bec0e940
Allen Rohner <arohner@gmail.com>
2011-10-17 15:04:46 -0500

    Pallet improvements to use SSL+passwords on all DB connections

--- src/circle/backend/nodes/circle_db.clj
+++ src/circle/backend/nodes/circle_db.clj
@@ -27,26 +27,25 @@
    "circleDB"
    :node-spec (pallet.core/node-spec
                :hardware {:hardware-id "t1.micro"
-                          ;; :map-new-volume-to-device-name ["/dev/sdf" 1 true]
-                          :map-ebs-snapshot-to-device-name ["/dev/sdf" (ebs/latest-snapshot "DB backup") 1 true]} 
+                          ;;:map-new-volume-to-device-name ["/dev/sdf" 1 true]
+                          :map-ebs-snapshot-to-device-name ["/dev/sdf" (ebs/latest-snapshot DB-snapshot-name) 1 false]} 
                :image {:os-family :ubuntu
                        :os-version-matches "11.04"
                        :location-id "us-east-1"}
-               :network {:inbound-ports [22]})
+               :network {:security-groups ["DB"]})
    :phases {:bootstrap (pallet.phase/phase-fn
                         (automated-admin-user/automated-admin-user))
             :settings (pallet.phase/phase-fn
-                       (postgres/settings (postgres/settings-map {:version "8.4"
-                                                                  :options {:data_directory "/data/db/%s"}
+                       (postgres/settings (postgres/settings-map {:options {:data_directory "/data/db/%s"
+                                                                            :listen_addresses "*"
+                                                                            :ssl "on"}
                                                                   :permissions [{:connection-type "local" :database "all" :user "all" :auth-method "trust"}
                                                                                 {:connection-type "host" :database "all" :user "all" :ip-mask "127.0.0.1/32" :auth-method "trust"}
-                                                                                {:connection-type "host" :database "all" :user "all" :ip-mask "::1/128" :auth-method "trust"}]}))
+                                                                                {:connection-type "host" :database "all" :user "all" :ip-mask "::1/128" :auth-method "trust"}
+                                                                                {:connection-type "hostssl" :database "all" :user "circle" :ip-mask "0.0.0.0/0" :auth-method "md5"}]}))
                        (parameter/update-target-settings :postgresql nil update-in [:clusters :main :permissions] #(drop 2 %)) ;; currently no easy way to remove the first two default auth settings, so remove them manually.
                        )
             :configure (pallet.phase/phase-fn
-                        ((fn [session]
-                           (println "*** Postgresql settings:" (parameter/get-target-settings session :postgresql nil))
-                          session))
                         (package/package-source "ubuntu-archive"
                                                 ;; by default, EC2
                                                 ;; ubuntu images only
@@ -64,14 +63,35 @@
                         (exec-script/exec-script "ls /dev/xvd*")
                         (exec-script/exec-script (mount "/dev/xvdf" "/data"))
                         (directory/directory "/data/db" :action :create :path true)
+
+                        ;; packages get installed early in the
+                        ;; process, regardless of the location of
+                        ;; (postgres). On ubuntu, services are started
+                        ;; at install time. We're going to change the
+                        ;; hba.conf, and the data directory, both
+                        ;; require restarts. postgres gets pissy if
+                        ;; you change the directory and then attempt
+                        ;; to restart. So just stop it now.
+                        
+                        (postgres/service :action :stop)
                         (postgres/postgresql-conf)
                         (postgres/hba-conf)
+                        (postgres/service-config)
                         (postgres/postgres)
-                        (postgres/initdb)
-                        (exec-script/exec-script (pkill postgres))
-                        (postgres/service :action :restart)
+                        (remote-file/remote-file "/data/db/main/server.key" :local-file "db-ssl.key"
+                                                 :owner "postgres"
+                                                 :group "postgres"
+                                                 :mode "600")
+                        (remote-file/remote-file "/data/db/main/server.crt" :local-file "db-ssl.crt"
+                                                 :owner "postgres"
+                                                 :group "postgres"
+                                                 :mode "600")
+                        ;; (postgres/initdb)
+                        (postgres/service :action :start)
+                        (pallet.crate.network-service/wait-for-port-listen 5432)
+                                                
                         (postgres/create-database "circleci")
-                        ;;users
+                        (postgres/create-role "circle" :user-parameters [:login :encrypted :password (format "'%s'" (-> circle.db/db-map :password))])
                         (user/user "circle"
                                    :action :create
                                    :shell :bash
@@ -88,7 +108,6 @@
                                              (slurp "www.id_rsa.pub"))
                         (ssh-key/authorize-key "circle"
                                                (slurp "www.id_rsa.pub"))
-                        (pallet.crate.network-service/wait-for-port-listen 5432)
                         (remote-file/remote-file "/home/circle/.ssh/config" :content "Host github.com\n\tStrictHostKeyChecking no\n"
                                                  :owner "circle"
                                                  :group "circle"
