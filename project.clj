a758cd65a0525155c13e0a0fef84a78948562010
Allen Rohner <arohner@gmail.com>
2011-10-19 12:56:33 -0500

    Organize the project.clj, add clojure.tools.logging

--- project.clj
+++ project.clj
@@ -3,27 +3,38 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-json "0.4.0"] ;; noir pulls in clj-json 0.3.2 which isn't compatible w/ clojure 1.3. Put this dep ahead to pull it in first.
+
+                 ;; Web
                  [noir "1.1.1-SNAPSHOT"]
-                 [clj-table "0.1.5"]
                  [clj-url "1.0.2"]
-                 [c3p0 "0.9.1.2"]
-                 [swank-clojure "1.3.2"]
-                 [log4j "1.2.14"]
-                 [log4j/apache-log4j-extras "1.1"]
                  [clj-http "0.2.1"]
                  [clj-r53 "1.0.1"]
+                 [commons-email "1.1"]
+                 [org.danlarkin/clojure-json "1.2-SNAPSHOT"]
+                 [com.amazonaws/aws-java-sdk "1.2.7"]
+                 
+                 ;; DB
+                 [clj-table "0.1.5"]
+                 [c3p0 "0.9.1.2"]
+                 
+                 ;; Logging
+                 [log4j "1.2.14"]
+                 [log4j/apache-log4j-extras "1.1"]
+                 [org.slf4j/slf4j-api "1.6.2"]
+                 [org.slf4j/slf4j-log4j12 "1.6.2"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 
+                 ;; Misc
+                 [swank-clojure "1.3.2"]
                  [commons-codec "1.4"]
                  [org.apache.commons/commons-compress "1.0"]
                  [org.xeustechnologies/jtar "1.0.4"]
-                 [commons-email "1.1"]
                  [arohner-utils "0.0.3"]
                  [clj-yaml "0.3.1"]
-                 [org.danlarkin/clojure-json "1.2-SNAPSHOT"]
-                 [com.amazonaws/aws-java-sdk "1.2.7"]
                  [fs "0.9.0"]
-                 
-                 [org.cloudhoist/pallet "0.6.5-SNAPSHOT"]
 
+                 ;; Pallet/jClouds
+                 [org.cloudhoist/pallet "0.6.5-SNAPSHOT"]
                  [org.jclouds/jclouds-core "1.2.0"]
                  [org.jclouds/jclouds-compute "1.2.0"]
                  [org.jclouds/jclouds-blobstore "1.2.0"]
@@ -42,6 +53,7 @@
                  [lein-crate "0.1.0"]
                  [lein-daemon "0.4.1"]
                  [pallet-rvm "0.1"]]
+  
   :repositories {"sonatype-releases" "http://oss.sonatype.org/content/repositories/releases"
                  "sonatype-snapshots" "http://oss.sonatype.org/content/repositories/snapshots"}
   :dev-dependencies [[lein-test-out "0.1.1"]
