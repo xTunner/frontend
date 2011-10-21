b235e97915456aee348b2e7c6bf53e850127000b
Paul Biggar <paul.biggar@gmail.com>
2011-10-21 00:02:17 -0700

    Expand test coverage of web site.

--- test/circle/web/test_web.clj
+++ test/circle/web/test_web.clj
@@ -1,11 +1,26 @@
 (ns circle.web.test-web
   (:require circle.init)
   (:require [clj-http.client :as http])
+  (:require [clj-http.core :as core])
   (:use midje.sweet))
 
 (circle.init/init)
 
 (def site "http://localhost:8080")
 
+
+
 (fact "/ returns 200"
-  (http/get site) => (contains {:status 200}))
+  (let [response (http/get site)]
+    response => (contains {:status 200 :body #"Sign up"})))
+
+(fact "posting works"
+      (let [post-request {:form-params {:email "mytest@test.com" :contact true}}
+            post-response (http/post site post-request)
+            get-request {:cookies (post-response :cookies)
+                         :url (get-in post-response [:headers "location"])}
+            get-response (http/get site get-request)]
+
+        post-response => (contains {:status 302})
+        (:headers post-response) => (contains {"location" "/"})
+        get-response => (contains {:status 200 :body #"Thanks"})))
