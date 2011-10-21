586806f2bd5faac8f89e99f2e4148f7295a8a3f3
Allen Rohner <arohner@gmail.com>
2011-10-20 20:18:13 -0500

    Move tests into test/, hopefully resolve midje weirdness

--- src/circle/utils/args.clj
+++ src/circle/utils/args.clj
@@ -1,6 +1,5 @@
 (ns circle.utils.args
-  (:use [circle.utils.except :only (throw-if-not)])
-  (:use [midje.sweet]))
+  (:use [circle.utils.except :only (throw-if-not)]))
 
 (defmacro require-arg
   "Throws if the argument is nil"
@@ -14,22 +13,3 @@
                 `(require-arg ~arg))]
     `(do
        ~@forms)))
-
-(defn f [foo]
-  (require-arg foo)
-  foo)
-
-(defn g [foo bar]
-  (require-args foo bar)
-  (+ foo bar))
-
-(fact "require-arg works"
-  (f 2) => 2
-  (f nil) => (throws Exception "foo is required"))
-
-(fact "require-args works"
-  (g 1 2) => 3
-  (g 1 nil) => (throws Exception "bar is required")
-  (g nil 2) => (throws Exception "foo is required")
-  (g nil nil) => (throws Exception))
-
