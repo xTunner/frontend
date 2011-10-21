d21d3771c37a4e8b36e490dd48290107434220de
Paul Biggar <paul.biggar@gmail.com>
2011-10-21 00:10:56 -0700

    Test the web site bodies more.

--- test/circle/web/test_web.clj
+++ test/circle/web/test_web.clj
@@ -8,19 +8,26 @@
 
 (def site "http://localhost:8080")
 
-
-
 (fact "/ returns 200"
-  (let [response (http/get site)]
-    response => (contains {:status 200 :body #"Sign up"})))
+  (let [response (http/get site)
+        body (response :body)]
+    response => (contains {:status 200})
+    body => #"Sign up"
+    body => #"Continuous Integration made easy"
+    body => #"form"
+    body =not=> #"Thanks"
+    ))
 
 (fact "posting works"
       (let [post-request {:form-params {:email "mytest@test.com" :contact true}}
             post-response (http/post site post-request)
             get-request {:cookies (post-response :cookies)
                          :url (get-in post-response [:headers "location"])}
-            get-response (http/get site get-request)]
+            get-response (http/get site get-request)
+            body (get-response :body)]
 
         post-response => (contains {:status 302})
         (:headers post-response) => (contains {"location" "/"})
-        get-response => (contains {:status 200 :body #"Thanks"})))
+        get-response => (contains {:status 200})
+        body => #"Thanks"
+        body =not=> #"form"))
