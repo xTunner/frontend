826e7ca45eb4cb77032a9ab7e31df1c1f20a941e
Paul Biggar <paul.biggar@gmail.com>
2011-10-23 00:11:30 -0700

    Test adding to the DB.

--- test/circle/web/test_web.clj
+++ test/circle/web/test_web.clj
@@ -3,8 +3,7 @@
   (:require [clj-http.client :as http])
   (:require [clj-http.core :as core])
   (:require [uuid])
-  (:use [circle.db :only (with-conn)])
-  (:require [circle.model.beta-notify :as beta])
+  (:require [somnium.congomongo :as mongo])
   (:use midje.sweet))
 
 (def site "http://localhost:8080")
@@ -16,30 +15,25 @@
     body => #"Sign up"
     body => #"Continuous Integration made easy"
     body => #"form"
-    body =not=> #"Thanks"
-    ))
+    body =not=> #"Thanks"))
 
 (fact "posting works"
   (let [session (uuid/uuid)
         email (str session "@test.com")
-;        db-entries-before (with-conn beta/find-one :where {:email email})
+        db-entries-before (mongo/fetch-count :signups :where {:email email})
         post-request {:form-params {:email email :contact true}}
         post-response (http/post site post-request)
         get-request {:cookies (post-response :cookies)
                      :url (get-in post-response [:headers "location"])}
         get-response (http/get site get-request)
         body (get-response :body)
-;        db-entries-after (with-conn beta/find-one :where {:email email})
-        ]
+        db-entries-after (mongo/fetch-count :signups :where {:email email})]
 
     post-response => (contains {:status 302})
     (:headers post-response) => (contains {"location" "/"})
     get-response => (contains {:status 200})
     body => #"Thanks"
     body =not=> #"form"
-;    (println db-entries-after)
-;    (println db-entries-after)
-;    (count db-entries-before) => 0
-;    (count db-entries-after) => 1
-    ))
+    db-entries-before => 0
+    db-entries-after => 1))
 
