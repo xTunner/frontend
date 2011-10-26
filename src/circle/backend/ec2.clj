5801ac0e0e2e4e927dcefad78a581ae045a79a05
Allen Rohner <arohner@gmail.com>
2011-10-26 10:38:50 -0500

    After catching the 'no such instance exception', don't NPE

--- src/circle/backend/ec2.clj
+++ src/circle/backend/ec2.clj
@@ -159,7 +159,7 @@
                    ;; started.
                    (when (not= "InvalidInstanceID.NotFound" (.getErrorCode e)) 
                      (throw e))))
-          state (-> inst :state (bean) :name (keyword))
+          state (-?> inst :state (bean) :name (keyword))
           ip (-> inst :publicIpAddress)
           sleep-interval 5]
       (println "block-until-running:" instance-id state)
