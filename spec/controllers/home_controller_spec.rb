29ba2b0bc2952ab2c4f91783786702d827c5f48b
Paul Biggar <paul.biggar@gmail.com>
2011-11-13 22:37:14 -0800

    Add a test for the title of the homepage.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe HomeController do
+  render_views
 
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
       response.should be_success
+      response.should have_selector("title", :content => "Circle - Continuous Integration made easy")
     end
   end
-
 end
