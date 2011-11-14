a7c0478e23a81894bdb6d0b3cbb8653d0559026b
Paul Biggar <paul.biggar@gmail.com>
2011-11-13 22:36:07 -0800

    Remove JRClj testing code from Home controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
 
-    clj = JRClj.new
-    clj.inc 0
+#    clj = JRClj.new
+#   #  clj.inc 0
 
-    circle = JRClj.new "circle.init"
+    # circle = JRClj.new "circle.init"
 
-    db = JRClj.new "circle.db"
-    db.run "circle.db/init"
+    # db = JRClj.new "circle.db"
+    # db.run "circle.db/init"
 
-    circle.run "circle.init/-main"
-    circle.init
+    # circle.run "circle.init/-main"
+    # circle.init
 
-    JRClj.new("circle.util.time").ju_now
+    # JRClj.new("circle.util.time").ju_now
   end
 end
