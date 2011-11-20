761532368be2b78413c18238faf66e0599b962a6
Paul Biggar <paul.biggar@gmail.com>
2011-11-19 17:32:39 -0800

    Add a signup model test.

--- spec/models/signup_spec.rb
+++ spec/models/signup_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Signup do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should handle a normal case" do
+    Signup.create!({:email => "anemail@asd.com", :contact => "true"})
+  end
+
+  it "shouldn't fail if it's not normal" do
+    Signup.create!({})
+    Signup.create!({:other => 'x'})
+    Signup.create!({:email => 5})
+    Signup.create!({:contact => true})
+  end
+
 end
