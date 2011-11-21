2cfff38de0d186fd70b82d4ab244d4fdd0e8306a
Allen Rohner <arohner@gmail.com>
2011-11-21 08:27:27 -0800

    Add missing require

--- backend/src/circle/backend/action/tag.clj
+++ backend/src/circle/backend/action/tag.clj
@@ -1,5 +1,6 @@
 (ns circle.backend.action.tag
   (:use [circle.backend.action :only (defaction)])
+  (:use [circle.util.except :only (throw-if-not)])
   (:require [circle.backend.nodes :as nodes])
   (:require [circle.backend.ec2-tag :as tag]))
 
