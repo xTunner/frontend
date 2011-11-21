b112ba235e68940fa54c5c5b9d3137b2e11daa0d
Allen Rohner <arohner@gmail.com>
2011-11-20 20:43:14 -0800

    Move last-{local,remote}-commit fns into backend.git. Make ssh-key optional.

--- backend/src/circle/env.clj
+++ backend/src/circle/env.clj
@@ -17,13 +17,3 @@
 (when production?
   (alter-var-root (var midje.semi-sweet/*include-midje-checks*) (constantly false))
   (alter-var-root (var clojure.test/*load-tests*) (constantly false)))
-
-(defn last-local-commit []
-  (->
-   (sh "git" "log" "-1" "--pretty=format:%H")
-   :out))
-
-(defn last-remote-commit []
-  (->
-   (sh "git" "log" "-1" "--pretty=format:%H" "origin/master")
-   :out))
