27c4d5bff99ab330e5d031adf9be5d818bdeaa9d
Paul Biggar <paul.biggar@gmail.com>
2011-11-26 22:34:14 -0800

    Actually call the hook from the front-end.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,12 @@
+require 'backend'
+
 class JobsController < ApplicationController
   respond_to :html, :json
 
   def create
     @project = Project.find(params[:project_id])
     @job = @project.jobs.create!
+    Backend.build(@project)
 
     respond_with(@project, @job)
   end
