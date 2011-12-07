e1b40ecaca8742f1db46f34021f3a21519cdcfb4
Allen Rohner <arohner@gmail.com>
2011-12-06 16:02:21 -0800

    Another missing file

---
+++ backend/src/circle/util/mongo.clj
@@ -0,0 +1,29 @@
+(ns circle.util.mongo
+  "Fns for working with mongo"
+  (:import org.bson.types.ObjectId)
+  (:use [arohner.utils :only (inspect)]))
+
+(defn object-id
+  "Generate a new object id, return it."
+  []
+  (ObjectId.))
+
+(defn object-id?
+  "True if map m has a mongo id"
+  [m]
+  (boolean (-> m :_id)))
+
+(defn ensure-object-id
+  "Adds a mongo id to m, a clojure map, if it doesn't have one."
+  [coll m]
+  {:post [(identity %)]}
+  (if (not (object-id? m))
+    (assoc m :_id (object-id))
+    m))
+
+(defn ensure-object-id-ref
+  "Same as ensure-object-id, but works on refs of maps."
+  [coll m]
+  (dosync
+   (when (not (object-id? @m))
+     (alter m (constantly (ensure-object-id coll @m))))))
