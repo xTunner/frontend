2e284434503ab36ca6e731fee04d053aaa6149fd
Paul Biggar <paul.biggar@gmail.com>
2011-12-12 01:13:17 -0800

    Make devise use the "outer application" layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
+
+  protected
+
+  # Specify the layout for the controller; rails looks for a layout with the
+  # name of the controller first, and only comes here when it can't find it. For
+  # devise, we use the "outer application" layout, currently named "join" :-/
+  def layout_by_resource
+    if devise_controller?
+      "join"
+    else
+      "application"
+    end
+  end
+
 end
