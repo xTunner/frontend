85bbb5f08c856b1944a4f43e00a434d68cb46ed9
Paul Biggar <paul.biggar@gmail.com>
2011-12-14 23:29:13 -0800

    Force SSL.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,9 @@
 MongoidTest::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Automatically promote http to https
+  config.force_ssl
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
