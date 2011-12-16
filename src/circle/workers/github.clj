516ff0742f7eacfe044680d28cc01eb6de939b12
Allen Rohner <arohner@gmail.com>
2011-12-15 16:02:23 -0800

    Move workers/github tests into the test tree. Fixes a bug in the github commit hook when in production

--- src/circle/workers/github.clj
+++ src/circle/workers/github.clj
@@ -12,9 +12,7 @@
   (:require [tentacles.repos :as trepos])
   (:require [tentacles.users :as tusers])
   (:require [tentacles.orgs :as torgs])
-  (:require circle.backend.build.test-utils)
-  (:require [circle.backend.ssh :as ssh])
-  (:use [midje.sweet]))
+  (:require [circle.backend.ssh :as ssh]))
 
 (defn process-json [github-json]
   (infof "build-hook: %s" github-json)
@@ -26,13 +24,6 @@
 (defn start-build-from-hook
   [url after ref json-string]
   (-> json-string json/decode process-json))
-
-(fact "start-build-from-hook works with dummy project"
-  (circle.backend.build.test-utils/ensure-test-project)
-  (let [json "{\"before\":\"5aef35982fb2d34e9d9d4502f6ede1072793222d\",\"repository\":{\"url\":\"https://github.com/arohner/circle-dummy-project\",\"name\":\"github\",\"description\":\"You're lookin' at it.\",\"watchers\":5,\"forks\":2,\"private\":1,\"owner\":{\"email\":\"chris@ozmm.org\",\"name\":\"defunkt\"}},\"commits\":[{\"id\":\"78f58846a049bb6772dcb298163b52c4657c7d45\",\"url\":\"https://github.com/arohner/circle-dummy-project/commit/78f58846a049bb6772dcb298163b52c4657c7d45\",\"author\":{\"email\":\"arohner@gmail.com\",\"name\":\"Allen Rohner\"},\"message\":\"okay i give in\",\"timestamp\":\"2008-02-15T14 =>57 =>17-08 =>00\",\"added\":[\"filepath.rb\"]}],\"after\":\"de8251ff97ee194a289832576287d6f8ad74e3d0\",\"ref\":\"refs/heads/master\"}"
-        build (start-build-from-hook nil nil nil json)]
-    (-> build (build/successful?)) => true
-    (-> @build :build_num) => integer?))
 
 ;;; https://github.com/account/applications/4808
 (def production-github {:client_id "78a2ba87f071c28e65bb"
@@ -86,10 +77,6 @@
                                                     :scope "repo"
                                                     :redirect_uri redirect})]
     (str endpoint "?" query-string)))
-
-(fact "authorization-url works"
-  (-> "http://localhost:3000/hooks/repos" authorization-url) =>
-  "https://github.com/login/oauth/authorize?client_id=586bf699b48f69a09d8c&scope=repo&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fhooks%2Frepos")
 
 (defn add-user-details [userid]
   (let [user (mongo/fetch-by-id :users (mongo/object-id userid))
