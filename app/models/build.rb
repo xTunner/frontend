dbf9d3bd1a1490c3b33ce1af1d6b0b4d4893d47c
Allen Rohner <arohner@gmail.com>
2011-12-16 22:04:37 -0800

    Add Rails model for builds

---
+++ app/models/build.rb
@@ -0,0 +1,7 @@
+## The canonical Build model lives in the clojure code. This definition exists to make Rail's querying easier
+class Build
+  include Mongoid::Document
+
+  field :vcs_url
+  field :start_date
+end
