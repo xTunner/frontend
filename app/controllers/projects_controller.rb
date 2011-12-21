4e1e562231006fbd537db11ff4eec7a8c6842433
Paul Biggar <paul.biggar@gmail.com>
2011-12-21 15:50:16 -0800

    load_and_authorize_resource doesnt do anything for projects anymore, so remove it.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,5 @@
 class ProjectsController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource
 
   def show
     url = Backend.blocking_worker "circle.backend.github-url/canonical-url", params[:project]
