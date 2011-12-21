2b92737d04e5611ea5904243263124c184daa727
Paul Biggar <paul.biggar@gmail.com>
2011-12-20 22:01:45 -0800

    Well, we gave everyone access to every project. Whoops.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,8 @@ class ProjectsController < ApplicationController
     url = Backend.blocking_worker "circle.backend.github-url/canonical-url", params[:project]
     @project = Project.where(vcs_url: url).first
 
+    authorize! :read, @project
+
     # TECHNICAL_DEBT: projects should have a list of builds, but it doesnt on the clojure side.
     @recent_builds = Build.where(vcs_url: @project.vcs_url).desc.limit(20)
 
