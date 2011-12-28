5eb83d2784470f346208d5031b14a31cef71d234
Paul Biggar <paul.biggar@gmail.com>
2011-12-27 16:53:07 -0800

    Add intercom. It's amazing.

--- spec/controllers/join_controller_spec.rb
+++ spec/controllers/join_controller_spec.rb
@@ -6,4 +6,11 @@ describe JoinController do
     pending
   end
 
+  describe "log in" do
+    login_user
+    it "sign_up_at should be very very recent" do
+      subject.current_user.sign_in_count.should == 0
+      subject.current_user.signup_at.should > (Time.now - 1)
+    end
+  end
 end
