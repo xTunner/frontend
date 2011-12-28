024498e90eecf2a60467a25a392fc224d220fabd
Paul Biggar <paul.biggar@gmail.com>
2011-12-27 16:57:43 -0800

    Add a check that a CanCan authorization happens in each controller action.
    This protects us from accidentally omitting the protect some data from a user.
    However, it doesn't protect us against getting the cancan permissions wrong.
    Note that we can skip permissions checks using "skip_authentication".

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  layout :layout_by_resource
+  protect_from_forgery # CXRF protection
 
-  protected
+  # dont render any action that cancan hasn't authorized.
+  check_authorization :unless => :devise_controller?
+
 
   # Specify the layout for the controller; rails looks for a layout with the
   # name of the controller first, and only comes here when it can't find it. For
   # devise, we use the "outer application" layout, currently named "join" :-/
+  layout :layout_by_resource
+  protected
   def layout_by_resource
     if devise_controller?
       "join"
