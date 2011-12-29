441f990238bb042400ab965f6734c731ec18b945
Paul Biggar <paul.biggar@gmail.com>
2011-12-29 01:46:01 -0800

    We no longer need to visible field then.

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,7 +8,6 @@ class Project
   field :vcs_url
   field :ssh_private_key
   field :ssh_public_key
-  field :visible, :type => Boolean, :default => false
 
   has_and_belongs_to_many :users
   has_many :builds
