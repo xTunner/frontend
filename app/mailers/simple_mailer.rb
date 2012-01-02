9f362abe58cc63ffa92b9607acbdb061ccb90d2c
Paul Biggar <paul.biggar@gmail.com>
2012-01-02 01:43:18 -0800

    Move test email into fail.html.haml, and add a success email.

--- app/mailers/simple_mailer.rb
+++ app/mailers/simple_mailer.rb
@@ -8,25 +8,27 @@ class SimpleMailer < ActionMailer::Base
   # - decide what type of email (depends on the branch, previous success)
   # - email them
   def build_email(build_id)
-
-    mail :to => "paul@circleci.com", :subject => "test"
+    return unless Rails.env.development?
 
     @build = Build.find(build_id)
+    @users = @build.the_project.users
+    @emails = @users.map { |u| u.email }.filter { |e| e.include? "@" }
 
     if @build.failed?
+      @logs = @build.logs
+      @failing_log = @logs.last
+      @other_logs = @logs[0..-1]
+      raise unless @failing_log.success?
+      mail(:to => @emails, :subject => "Your build failed!").deliver
       render "fail"
     else
+      mail(:to => @emails, :subject => "Your build succeeded").deliver
       render "success"
     end
-
   end
 
   def test_email
     @build = Build.all.last
-    @logs = @build.logs
-    @failing_log = @logs.last
-    @other_logs = @logs[0..-1]
-    raise unless @failing_log.success?
-    mail(:to => "paul@circleci.com", :subject => "a test email").deliver
+    build_email(@build.id)
   end
 end
