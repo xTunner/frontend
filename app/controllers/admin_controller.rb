ba4a512829aed1f2bdc3932282bf9af4a4dcf5b2
Paul Biggar <paul.biggar@gmail.com>
2012-01-01 22:16:43 -0800

    Fix the openssl mail problem. This works for actionmailer but it's not a real fix. The real fix should be forthcoming in jruby 1.6.6.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,21 @@
+# HTTPS fails in JRuby --1.9, this works around it until they fix it in 1.6.6.
+# http://jira.codehaus.org/browse/JRUBY-5529
+# https://gist.github.com/969527
+Net::BufferedIO.class_eval do
+  BUFSIZE = 1024 * 16
+
+  def rbuf_fill
+    timeout(@read_timeout) {
+      @rbuf << @io.sysread(BUFSIZE)
+    }
+  end
+end
+
 class AdminController < ApplicationController
   before_filter :authenticate_user!
   authorize_resource :class => false
 
   def show
-
     @projects = Project.order_by([[:vcs_url, :asc]])
     @builds = Build.order_by([[:start_time, :desc]]).limit(20).all
   end
