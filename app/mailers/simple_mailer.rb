9ef8c0195d994bab6ce7d8e6ca262f1d93d8b1cd
Paul Biggar <paul.biggar@gmail.com>
2012-01-02 01:13:16 -0800

    Flesh out the test email.

--- app/mailers/simple_mailer.rb
+++ app/mailers/simple_mailer.rb
@@ -1,5 +1,7 @@
 class SimpleMailer < ActionMailer::Base
   default :from => "notifications@circleci.com"
+  include ApplicationHelper
+  helper :application
 
   # Sends an email for a build:
   # - work out who should be emailed based on their preferences
@@ -21,6 +23,10 @@ class SimpleMailer < ActionMailer::Base
 
   def test_email
     @build = Build.all.last
+    @logs = @build.logs
+    @failing_log = @logs.last
+    @other_logs = @logs[0..-1]
+    raise unless @failing_log.success?
     mail(:to => "paul@circleci.com", :subject => "a test email").deliver
   end
 end
