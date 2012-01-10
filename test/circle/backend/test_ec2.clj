31b1e3e3ef7ccf3fbd2a1436e2f05aa9ac99291c
Allen Rohner <arohner@gmail.com>
2012-01-09 18:47:39 -0800

    Adds ec2/safe-terminate. Use it when running builds

---
+++ test/circle/backend/test_ec2.clj
@@ -0,0 +1,17 @@
+(ns circle.backend.test-ec2
+  (:use circle.backend.ec2)
+  (:require [circle.backend.load-balancer :as lb])
+  (:use midje.sweet))
+
+(fact "safe terminate doesn't terminate instances attached to the load balancer"
+  (safe-terminate "i-production") => nil
+  (provided
+    (terminate-instances! anything) => anything :times 0
+    (lb/instances "www") => ["i-production"]))
+
+(fact "safe terminate kills unattached instances"
+  (safe-terminate "i-devel") => nil
+  (provided
+    (terminate-instances! "i-devel") => nil :times 1)
+  (provided
+    (lb/instances "www") => ["i-production"]))
