3f5ee0130b950de698882d52f570c91507b2e8ea
Allen Rohner <arohner@gmail.com>
2012-01-11 11:58:44 -0800

    More missing files from improved inference

---
+++ src/circle/db/analysis.clj
@@ -0,0 +1,56 @@
+(ns circle.db.analysis
+  "Utility functions for the DB"
+  (:require [clojure.string :as str])
+  (:require [somnium.congomongo :as mongo])
+  (:require [clojure.set :as set]))
+
+(defn spec-distinct-lines [spec]
+  (-> spec
+      (select-keys [:setup :dependencies :compile :test])
+      (vals)
+      (->>
+       (mapcat #(str/split % #"\r\n"))
+       (filter #(> (.length %) 0))
+       (map (fn [s] {s 1})))))
+
+(defn all-spec-lines []
+  (->> (mongo/fetch :specs)
+       (mapcat spec-distinct-lines)))
+
+(defn spec-histogram
+  "returns a histogram of unique lines in the spec files"
+  []
+  (->> (all-spec-lines)
+       (apply merge-with +)
+       (seq)
+       (sort-by second)
+       (reverse)))
+
+(defn projects-with
+  "Returns a list of project urls that contain spec lines that match the regex"
+  [re]
+  (->> (mongo/fetch :specs)
+       (map (fn [spec]
+              (let [project (mongo/fetch-one :projects :where {:_id (-> spec :project_id)})]
+                {:project (-> project :vcs_url)
+                 :lines (->> spec
+                             (spec-distinct-lines )
+                             (mapcat keys)
+                             (filter #(re-find re %)))})))
+       (filter (fn [{:keys [project lines]}]
+                 (->> lines
+                      (seq)
+                      (boolean))))))
+
+(defn num-pure-inferred-projects
+  "Returns a list of projects purely inferred, and several builds"
+  []
+  (let [spec-project-ids (->> (mongo/fetch :specs) (map :project_id) (into #{}))
+        project-ids (->> (mongo/fetch :projects)
+                          (map :_id)
+                          (into #{}))
+        pure-inferred (set/difference project-ids spec-project-ids)]
+    (println (count pure-inferred) (first pure-inferred))
+    (->> pure-inferred
+         (map #(mongo/fetch-one :projects :where {:_id %}))
+         (map :vcs_url))))
