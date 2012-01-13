0968115ef2306adbb084342c9dc2999a0184afc2
Paul Biggar <paul.biggar@gmail.com>
2012-01-13 11:35:04 -0800

    Regrettably, there are builds with no start_time!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,6 +41,7 @@ module ApplicationHelper
   end
 
   def as_length_of_build(start, stop)
+    return if start.nil?
     # TECHNICAL_DEBT: we should add stop_times for all builds, and then update the DB
     if stop == nil
       stop = Time.now
