7d987a6228538318cbfba055bf1b2ef7f4f4b2d2
Allen Rohner <arohner@gmail.com>
2012-01-13 14:29:51 -0800

    Beginnings of the gems map file.

---
+++ src/circle/backend/build/inference/gems_map.clj
@@ -0,0 +1,7 @@
+(ns circle.backend.build.inference.gems-map)
+
+(def gem-package-map
+  ;; this is a map of rubygems to the ubuntu packages necessary for this gem to install. Incomplete.
+  {"memcached" #{"libmemcached-dev" "libsasl2-dev"}
+   "cdamian-geoip_city" #{"libgeoip-dev"}}
+  )
