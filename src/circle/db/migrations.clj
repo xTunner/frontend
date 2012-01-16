5f09ef76b01116552b7e0d9161c8eb31e818c09c
Allen Rohner <arohner@gmail.com>
2012-01-15 19:36:15 -0800

    Adds migration to move specs into projects. Update the projects controller accordingly.

--- src/circle/db/migrations.clj
+++ src/circle/db/migrations.clj
@@ -1,6 +1,7 @@
 (ns circle.db.migrations
   (:require [clojure.set :as set])
   (:use circle.db.migration-lib)
+  (:require [somnium.congomongo :as mongo])
   (:use [circle.util.core :only (apply-if)]))
 
 (clear-migrations)
@@ -17,6 +18,14 @@
                (let [empty? (every? empty? ((juxt :dependencies :test :compile :setup) spec))]
                  (println spec "empty?=" empty?)
                  (apply-if empty? assoc spec :inferred true))))
+
+(def-migration "move specs into project"
+  :num 2
+  :coll :projects
+  :transform (fn [p]
+               (assert (= 1 (mongo/fetch :specs :where {:project_id (:_id p)} :count? true)))
+               (let [spec (mongo/fetch-one :specs :where {:project_id (:_id p)})]
+                 (apply-if spec merge p (select-keys spec [:dependencies :setup :compile :test])))))
 
 ;; (def-migration "old builds w/ git commit info")
 
