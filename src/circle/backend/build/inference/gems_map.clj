f5c8891d17e0a7a87784e6cff8a641023b4b0b15
Allen Rohner <arohner@gmail.com>
2012-01-18 15:13:14 -0800

    Update the rails AMI
    * Add apt configuration so apt-get doesn't prompt for input. Fixes #59
    * Add several ubuntu packages so customers don't need to specify them. Fixes #66

--- src/circle/backend/build/inference/gems_map.clj
+++ src/circle/backend/build/inference/gems_map.clj
@@ -3,5 +3,5 @@
 (def gem-package-map
   ;; this is a map of rubygems to the ubuntu packages necessary for this gem to install. Incomplete.
   {"memcached" #{"libmemcached-dev" "libsasl2-dev"}
-   "cdamian-geoip_city" #{"libgeoip-dev"}}
-  )
+   "cdamian-geoip_city" #{"libgeoip-dev"}
+   "capybara-webkit" #{"libqtwebkit-dev"}})
