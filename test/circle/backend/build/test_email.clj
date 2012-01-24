7e20fcf2b4f09e141123925b56a05443646e2374
Allen Rohner <arohner@gmail.com>
2012-01-24 17:09:09 -0600

    Refactor the email sending into two functions, so it can be tested without straight-jacket. Restore the sending email test

--- test/circle/backend/build/test_email.clj
+++ test/circle/backend/build/test_email.clj
@@ -4,31 +4,11 @@
   (:use [circle.backend.build.test-utils :only (minimal-build)])
   (:require [circle.backend.build.email :as email])
   (:use [circle.backend.action :only (defaction)])
-  (:require [circle.backend.build :as build])
+  (:require [circle.model.build :as build])
   (:use [circle.backend.build.run :only (run-build)])
   (:use circle.backend.build.config))
 
-(defaction successful-action [act-name]
-  {:name act-name}
-  (fn [build]
-    nil))
-
-(defn successful-build []
-  (build/build {:build_num 1
-                :vcs_url "git@github.com:foo/bar.git"
-                :vcs_revision "f00b4r"
-                :actions [(successful-action "1")
-                          (successful-action "2")
-                          (successful-action "3")]}))
-
-;;
-;; This test 'works', but we temporarily hard coded all emails to go to us, rather than customers, so the test fails.
-;;
-
-;; (fact "notify email works"
-;;   (build/successful? (run-build (minimal-build :actions [(successful-action "1")]
-;;                                                :notify_emails ["foo@bar.com"
-;;                                                               "baz@bar.com"]))) => truthy
-;;   (provided
-;;     (circle.backend.email/send :to "foo@bar.com" :subject anything :body anything) => anything :times 1
-;;     (circle.backend.email/send :to "baz@bar.com" :subject anything :body anything) => anything :times 1))
+(fact "notify email works"
+  (let [build (circle.backend.build.config/build-from-url (-> test/test-project :vcs_url))]
+    (build/successful? (run-build build)) => truthy
+    (circle.backend.build.email/notify-build-results* build) => truthy))
