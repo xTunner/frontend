9212f912ddb7364f948b1c0d540dcb255c25a88d
Paul Biggar <paul.biggar@gmail.com>
2012-02-02 20:42:56 -0800

    Fix some names were not updated after being moved to a different namespace.
    (I hate how Clojure doesn't clear it's namespaces when you recompile!)

--- test/circle/backend/build/inference/test_mysql.clj
+++ test/circle/backend/build/inference/test_mysql.clj
@@ -1,17 +1,17 @@
 (ns circle.backend.build.inference.test-mysql
   (:use midje.sweet)
-  (:use circle.backend.build.inference.rails)
-  (:use circle.backend.build.inference.test-rails)
+  (:require [circle.backend.build.test-utils :as test])
+  (:require [circle.backend.build.inference.rails :as rails])
   (:require fs)
   (:require [circle.backend.git :as git]))
 
 (fact "mysql-user"
-  (let [repo (test-repo "mysql_1")]
-    (-> (ensure-db-user repo) :command) => (contains #"mysql -u")
-    (need-mysql-socket? repo) => true
-    (need-mysql-socket? empty-repo) => false))
+  (let [repo (test/test-repo "mysql_1")]
+    (-> (rails/ensure-db-user repo) :command) => (contains #"mysql -u")
+    (rails/need-mysql-socket? repo) => true
+    (rails/need-mysql-socket? test/empty-repo) => false))
 
 (fact "mysql2 user"
-  (let [repo (test-repo "mysql_2")]
-    (-> (ensure-db-user repo) :command) => (contains #"mysql -u")
-    (need-mysql-socket? repo) => true))
+  (let [repo (test/test-repo "mysql_2")]
+    (-> (rails/ensure-db-user repo) :command) => (contains #"mysql -u")
+    (rails/need-mysql-socket? repo) => true))
--- test/circle/backend/build/inference/test_rails.clj
+++ test/circle/backend/build/inference/test_rails.clj
@@ -6,68 +6,68 @@
   (:require [circle.backend.git :as git]))
 
 (fact "bundler?"
-  (let [bundler-repo (test-repo "bundler_1")]
-    (bundler? bundler-repo) => true
-    (bundler? empty-repo) => false))
+  (let [repo (test/test-repo "bundler_1")]
+    (bundler? repo) => true
+    (bundler? test/empty-repo) => false))
 
 (fact "bundler action"
-  (let [bundler-repo (test-repo "bundler_1")]
-    (map :name (spec bundler-repo)) => (contains ["bundle install"])))
+  (let [repo (test/test-repo "bundler_1")]
+    (map :name (spec repo)) => (contains ["bundle install"])))
 
 (fact "rspec? works"
-  (let [rspec-repo (test-repo "rspec_1")]
-    (rspec? rspec-repo) => true
-    (rspec? empty-repo) => false))
+  (let [repo (test/test-repo "rspec_1")]
+    (rspec? repo) => true
+    (rspec? test/empty-repo) => false))
 
 (fact "rspec? doesn't trigger with no .rb files"
-  (let [rspec-repo (test-repo "rspec_empty")]
-    (rspec? rspec-repo) => false))
+  (let [repo (test/test-repo "rspec_empty")]
+    (rspec? repo) => false))
 
 (fact "test-unit? doesn't trigger with no .rb files"
-  (let [repo (test-repo "rake_test_empty")]
+  (let [repo (test/test-repo "rake_test_empty")]
     (test-unit? repo) => false))
 
 (fact "rspec uses bundler when appropriate"
-  (let [bundler-repo (test-repo "rspec_1")]
-    (->> (spec bundler-repo) (map :name) (into #{})) => (contains #"bundle exec rspec spec"))
-  (let [no-bundler-repo (test-repo "no_bundler_1")]
+  (let [repo (test/test-repo "rspec_1")]
+    (->>(spec repo) (map :name) (into #{})) => (contains #"bundle exec rspec spec"))
+  (let [no-bundler-repo (test/test-repo "no_bundler_1")]
     (->> (spec no-bundler-repo) (map :name)) => (contains "rspec spec")))
 
 (fact "rspec action"
-  (let [repo (test-repo "rspec_1")]
+  (let [repo (test/test-repo "rspec_1")]
     (->> (spec repo) (map :name) (into #{})) => (contains "bundle exec rspec spec")))
 
 (fact "rspec actions have type :test"
-  (let [repo (test-repo "rspec_1")]
+  (let [repo (test/test-repo "rspec_1")]
     (->> (spec repo) (map :type) (into #{})) => (contains :test)))
 
 (fact "db:create when db.yml action"
-  (let [repo (test-repo "database_yml_2")]
+  (let [repo (test/test-repo "database_yml_2")]
     (database-yml? repo) => true
     (->> (spec repo) (map :name) (into #{}))  => (contains "bundle exec rake db:create --trace")))
 
 (fact "inference finds database yaml"
-  (let [repo (test-repo "database_yml_1")]
+  (let [repo (test/test-repo "database_yml_1")]
     (database-yml? repo) => false
     (find-database-yml repo) => (fs/join repo "config/database.example.yml")))
 
 (fact "inference finds database yaml using 'default' instead of 'example'"
-  (let [repo (test-repo "database_yml_3")]
+  (let [repo (test/test-repo "database_yml_3")]
     (database-yml? repo) => false
     (find-database-yml repo) => (fs/join repo "config/database.yml.default")))
 
 (fact "data-mapper? works"
-  (let [repo (test-repo "dm_rails_1")]
+  (let [repo (test/test-repo "dm_rails_1")]
     (data-mapper? repo) => true
-    (data-mapper? empty-repo) => falsey))
+    (data-mapper? test/empty-repo) => falsey))
 
 (fact "call rake db:automigrate when using dm-rails"
-  (let [repo (test-repo "dm_rails_1")]
+  (let [repo (test/test-repo "dm_rails_1")]
     (spec repo)
     (->> (spec repo) (map :name) (into #{})) => (contains #"rake db:automigrate")))
 
 (fact "copy database.example.yml to database.yml action"
   ;; repo with database.example.yml
-  (let [example-repo (test-repo "database_yml_1")]
+  (let [example-repo (test/test-repo "database_yml_1")]
     (->> (spec example-repo) (map :name)) => (and (contains "copy database.yml")
                                                   (contains "rake db:create --trace"))))
