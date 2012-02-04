0f9617ad4bcef9f13861f5d9c28d91df35f1a69c
Paul Biggar <paul.biggar@gmail.com>
2012-02-03 17:15:19 -0800

    Use :force true instead of production so that the airbrake appears in the right category on the website, and won't bother us.

--- test/circle/util/test_airbrake.clj
+++ test/circle/util/test_airbrake.clj
@@ -5,6 +5,4 @@
 (def div-by-zero (try (/ 1 0) (catch Exception e e)))
 
 (fact "airbrake works"
-  (circle.airbrake/airbrake :exception div-by-zero) => (contains {:id integer? :error-id integer? :url string?})
-    (provided
-      (circle.env/env) => :production))
+  (airbrake :exception div-by-zero :force true) => (contains {:id integer? :error-id integer? :url string?}))
