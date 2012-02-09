2f502920848ec1b6407807c53d37587ee57bcb9a
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-02-09 11:55:26 -0800

    Add leiningen and clojure test projects.

---
+++ test/circle/backend/build/inference/test_dirs/midje_project/project.clj
@@ -0,0 +1,5 @@
+(defproject midje_project "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[midje "1.3.0"]])
+
---
+++ test/circle/backend/build/inference/test_dirs/midje_project/test/leiningen_project/test/core.clj
@@ -0,0 +1,6 @@
+(ns leiningen_project.test.core
+  (:use [leiningen_project.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
