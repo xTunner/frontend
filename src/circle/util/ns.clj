5a2b5aa7bf721656f02d67c6144b7db12fdbbea1
Allen Rohner <arohner@gmail.com>
2012-02-10 12:49:24 -0800

    Add circle.util.ns/destroy-ns

---
+++ src/circle/util/ns.clj
@@ -0,0 +1,16 @@
+(ns circle.util.ns
+  "fns for working with clojure namespaces")
+
+(defn destroy-ns
+  "TODO"
+  [ns]
+  (doseq [sym (keys (ns-refers ns))]
+    (ns-unmap ns sym)))
+
+(defmacro with-ns
+  "Evaluates body in another namespace.  ns is either a namespace
+  object or a symbol.  This makes it possible to define functions in
+  namespaces other than the current one."
+  [ns & body]
+  `(binding [*ns* (the-ns ~ns)]
+     ~@(map (fn [form] `(eval '~form)) body)))
