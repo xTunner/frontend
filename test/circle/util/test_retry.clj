c211785c85744b584546513868563a3598a196fe
Allen Rohner <arohner@gmail.com>
2012-02-15 11:01:16 -0800

    Speed up the retry tests, add more tests to try to pin down the EC2 infinite retry bug.

--- test/circle/util/test_retry.clj
+++ test/circle/util/test_retry.clj
@@ -6,7 +6,7 @@
 (defn foo [])
 
 (fact "wait-for retries"
-  (wait-for {:sleep 100
+  (wait-for {:sleep 1
              :tries 10}
             (fn []
               (foo))) => true
@@ -14,10 +14,32 @@
                 (foo) =streams=> [false false false false true] :times 5))
 
 (fact "wait-for throws on timeout"
-  (wait-for {:sleep 100
+  (wait-for {:sleep 1
              :tries 10}
             (fn []
               (foo))) => (throws Exception)
               (provided
                 (foo) => false :times 10))
+
+(fact "wait-for works with an error hook"
+  (wait-for {:sleep 1
+             :tries 10
+             :error-hook (fn [e]
+                           (printf (format "caught: %s\n" (.getMessage e))))}
+            (fn []
+              (foo))) => true
+              (provided
+                (foo) =streams=> [false false false false true] :times 5))
+
+(fact "wait-for works with a success fn"
+  (wait-for {:sleep 1
+             :tries 10
+             :success-fn (fn [v]
+                           (= v 8))}
+            (fn []
+              (foo))) => 8
+              (provided
+                (foo) =streams=> (range) :times 9))
+
+
 
