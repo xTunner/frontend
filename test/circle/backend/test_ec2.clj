00710d54b955e3656fb4e29d63ac111d2f7b112e
Allen Rohner <arohner@gmail.com>
2012-02-15 15:18:25 -0800

    moar tests!

--- test/circle/backend/test_ec2.clj
+++ test/circle/backend/test_ec2.clj
@@ -1,6 +1,7 @@
 (ns circle.backend.test-ec2
   (:use circle.backend.ec2)
   (:require [circle.backend.load-balancer :as lb])
+  (:use [circle.util.except :only (throwf)])
   (:use midje.sweet))
 
 (fact "safe terminate doesn't terminate instances attached to the load balancer"
@@ -15,4 +16,9 @@
     (terminate-instances! "i-devel") => nil :times 1)
   (provided
     (lb/instances "www") => ["i-production"]))
+
+(future-fact "block-until-running retries when instance returns nil"
+  (block-until-running "i-bogus") => (throws Exception)
+  (provided
+    (instance anything) =streams=> [(throwf "not started") (throwf "not started") (throwf "not started") :running]))
 
