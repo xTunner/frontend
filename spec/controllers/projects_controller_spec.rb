8d959cfa1c9289028e5883991246f1c4cf8f5d2b
Paul Biggar <paul.biggar@gmail.com>
2012-02-17 13:08:58 -0800

    Fix project tests.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,34 +1,39 @@
 require 'spec_helper'
 
 describe ProjectsController do
-  login_user # don't forget you can use current_user
-  uses_workers
-
+  use_clojure_factories
+  login_user
+  use_workers
 
   it "should render using pretty 'gh' links" do
-    project = FactoryGirl.create :project
+    project = Project.first
     github_project_path(project).should == '/gh/' + project.github_project_name
   end
 
   it "shouldn't be able to render unowned projects" do
-    project = FactoryGirl.create :unowned_project
+    project = Project.first
+    project.users = []
+    project.save!
+
     lambda {
       get 'show', :project => project.github_project_name
     }.should raise_error(CanCan::AccessDenied)
   end
 
   it "should be able to access owned projects" do
-    project = FactoryGirl.create :project
+    project = Project.first
     subject.current_user.projects << project
     get 'show', :project => project.github_project_name
   end
 
   it "should work for weird characters in the project url" do
-    github_project_path(FactoryGirl.create(:project_with_weird_characters))
+    project = Project.unsafe_create :vcs_url => "https://github.com/._-_.-/-.__-.-/"
+    github_project_path(project)
   end
 
   describe "json tests" do
-    let!(:project) { Project.unsafe_create :vcs_url => "https://github.com/arohner/circle-dummy-project", :users => [subject.current_user] }
+    disable_mocking
+
     before(:each) do
       request.env["HTTP_ACCEPT"] = "application/json"
     end
@@ -38,15 +43,12 @@ describe ProjectsController do
     end
 
     it "should start a build when the page is edited" do
-      Backend.mock = false
-
+      project = Project.from_url "https://github.com/arohner/circle-dummy-project"
       put :update, { :project => "arohner/circle-dummy-project", "setup" => "" }.as_json
       Backend.wait_for_all_workers
 
       project.latest_build.why.should == "edit"
       project.latest_build.user.should == subject.current_user
-
-      Backend.mock = true
     end
 
     it "should start a hipchat notification worker" do
