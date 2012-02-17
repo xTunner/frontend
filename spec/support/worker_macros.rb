c394eec1fd75dea5c45d3e13c1ebe5ab910db6e7
Paul Biggar <paul.biggar@gmail.com>
2012-02-17 12:15:34 -0800

    Add new spec support that uses the backend more.

--- spec/support/worker_macros.rb
+++ spec/support/worker_macros.rb
@@ -1,7 +1,27 @@
 module WorkerMacros
-  def uses_workers
+  def use_workers
     after(:each) do
+      old = Backend.mock
+      Backend.mock = false
       Backend.wait_for_all_workers
+      Backend.mock = old
+    end
+  end
+
+  def disable_mocking
+    before :each do
+      Backend.mock = false
+    end
+
+    after :each do
+      Backend.mock = true
+    end
+  end
+
+  def use_clojure_factories
+    before :each do
+      # Blocking workers skip the mock check
+      Backend.blocking_worker "circle.backend.build.test-utils/ensure-test-db"
     end
   end
 end
