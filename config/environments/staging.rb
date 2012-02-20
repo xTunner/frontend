918c41c401b2f5a2f200a105be9dae85b90a706f
Allen Rohner <arohner@gmail.com>
2012-02-20 12:34:47 -0800

    Make staging send email. (Mail safe is still enabled)

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,11 +68,10 @@ MongoidTest::Application.configure do
                                   shared/*.css
                                 )
 
-  # ActionMailer Config: staging - do not perform deliveries
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'staging.circleci.com' }
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   # Enable threaded mode
