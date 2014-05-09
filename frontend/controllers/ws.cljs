e0d0dcf6d0b538168e604dc1b30af068aa8da2f1
Daniel Woelfel <dwwoelfel@gmail.com>
2014-05-09 02:10:05 -0700

    adds append-output and update-action pusher handling

--- frontend/controllers/ws.cljs
+++ frontend/controllers/ws.cljs
@@ -38,3 +38,33 @@
                                                                           :actions []}))
             (update-in [:current-build] build-model/fill-actions step-index action-index action-log)
             (assoc-in [:current-build :steps step-index :actions action-index] action-log))))))
+
+(defmethod ws-event :build/update-action
+  [pusher-imp message {:keys [data channel-name]} state]
+  (let [build (:current-build state)]
+    (if-not (= (pusher/build-channel build) channel-name)
+      (do
+        (mlog "Ignoring event for old build channel: " channel-name)
+        state)
+      (let [{step-index :step action-index :index action-log :log} (js->clj data :keywordize-keys true)]
+        (-> state
+            (update-in [:current-build] build-model/fill-steps step-index)
+            (update-in [:current-build :steps step-index] (fnil identity {:name (:name action-log)
+                                                                          :actions []}))
+            (update-in [:current-build] build-model/fill-actions step-index action-index action-log)
+            (update-in [:current-build :steps step-index :actions action-index] merge action-log))))))
+
+(defmethod ws-event :build/append-action
+  [pusher-imp message {:keys [data channel-name]} state]
+  (let [build (:current-build state)]
+    (if-not (= (pusher/build-channel build) channel-name)
+      (do
+        (mlog "Ignoring event for old build channel: " channel-name)
+        state)
+      (let [{step-index :step action-index :index output :out} (js->clj data :keywordize-keys true)]
+        (-> state
+            (update-in [:current-build] build-model/fill-steps step-index)
+            (update-in [:current-build :steps step-index] (fnil identity {:actions []}))
+            (update-in [:current-build] build-model/fill-actions step-index action-index {})
+            (update-in [:current-build :steps step-index :actions action-index] (fnil identity {:output []}))
+            (update-in [:current-build :steps step-index :actions action-index :output] conj output))))))
