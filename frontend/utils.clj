a5d1185a73e4c5f40634d18532e654dbfb3ce1b7
Daniel Woelfel <dwwoelfel@gmail.com>
2014-05-12 23:25:18 -0700

    timing macro

--- frontend/utils.clj
+++ frontend/utils.clj
@@ -8,3 +8,13 @@
            result# ~value]
        (print (pr-str name#) "is" (pr-str result#))
        result#)))
+
+(defmacro timing
+  "Evaluates expr and prints the label and the time it took.
+  Returns the value of expr."
+  {:added "1.0"}
+  [label expr]
+  `(let [start# (.getTime (js/Date.))
+         ret# ~expr]
+     (prn (str ~label " elapsed time: " (- (.getTime (js/Date.)) start#) " msecs"))
+     ret#))
