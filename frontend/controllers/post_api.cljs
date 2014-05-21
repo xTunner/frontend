66e25ace3d7fba65e9474ae4c7711af0a1ef036f
Daniel Woelfel <dwwoelfel@gmail.com>
2014-05-21 00:53:24 -0700

    fetch failed action logs when the build loads

--- frontend/controllers/post_api.cljs
+++ frontend/controllers/post_api.cljs
@@ -2,6 +2,7 @@
   (:require [cljs.core.async :as async :refer [>! <! alts! chan sliding-buffer put! close!]]
             [clojure.string :as string]
             [frontend.intercom :as intercom]
+            [frontend.models.action :as action-model]
             [frontend.models.project :as project-model]
             [frontend.models.repo :as repo-model]
             [frontend.routes :as routes]
@@ -37,6 +38,19 @@
 (defmethod post-api-event! [:default :finished]
   [target message status args previous-state current-state]
   (mlog "No post-api for: " [message status]))
+
+(defmethod post-api-event! [:build :success]
+  [target message status args previous-state current-state]
+  (let [{:keys [build-num project-name]} (:context args)]
+    ;; This is slightly different than the api-event because we don't want to have to
+    ;; convert the build from steps to containers again.
+    (when (and (= build-num (get-in args [:resp :build_num]))
+               (= project-name (vcs-url/project-name (get-in args [:resp :vcs_url]))))
+      (doseq [action (mapcat :actions (get-in current-state [:current-build :containers]))
+              :when (action-model/failed? action)]
+        ;; XXX: should this fetch the action logs itself creating controls events?
+        (put! (get-in current-state [:comms :controls])
+              [:action-log-output-toggled (select-keys action [:step :index])])))))
 
 (defmethod post-api-event! [:followed-repo :success]
   [target message status args previous-state current-state]
