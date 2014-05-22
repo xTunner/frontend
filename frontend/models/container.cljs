81fc2bc160d7e8031f6a26787e2c5831d4ea13c6
Daniel Woelfel <dwwoelfel@gmail.com>
2014-05-21 18:58:19 -0700

    status class for the container pills

--- frontend/models/container.cljs
+++ frontend/models/container.cljs
@@ -1,12 +1,20 @@
 (ns frontend.models.container
-  (:require [frontend.datetime :as datetime]
-            [frontend.models.project :as proj]
+  (:require [clojure.set :refer (intersection)]
+            [frontend.datetime :as datetime]
             [goog.string :as gstring]
             goog.string.format))
 
 (defn id [container]
   (:index container))
 
-;; XXX: implement container status style
-(defn status-style [container]
-  "running")
+(defn status-classes [container]
+  (let [action-statuses (->> container :actions (map :status) (remove nil?) set)]
+    (concat []
+            (when (empty? action-statuses) ["running"])
+            (when (seq (intersection #{"failed" "timedout" "cancelled" "infrastructure_fail"}
+                                     action-statuses))
+              ["failed"])
+            (when (some :canceled (:actions container))
+              ["canceled"])
+            (when (= #{"success"} action-statuses)
+              ["success"]))))
