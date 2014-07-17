a0e5e6a510781c1fee7aaec036012d8508df18b3
Daniel Woelfel <dwwoelfel@gmail.com>
2014-07-17 15:50:35 -0700

    fix _gaq.push before the script has finished loading

--- frontend/analytics/google.cljs
+++ frontend/analytics/google.cljs
@@ -2,8 +2,9 @@
   (:require [frontend.utils :as utils :include-macros true]))
 
 (defn push [args]
-  (let [gaq (aget js/window "_gaq")]
-     ((aget gaq "push") (clj->js args))))
+  (let [gaq (aget js/window "_gaq")
+        push (aget gaq "push")]
+     (.call push gaq (clj->js args))))
 
 (defn track-event [& args]
   (utils/swallow-errors (push (cons "_trackEvent" args))))
