4078c2c8808d0dc7d1f999c73f7b5fe3c53fdfbb
worldsoup <nick@circleci.com>
2014-07-24 16:32:12 -0700

    some style and content

--- frontend/components/language_landing.cljs
+++ frontend/components/language_landing.cljs
@@ -25,7 +25,13 @@
                         :ss-1 "/assets/img/outer/languages/ruby-ss-1.png"
                         :ss-2 "/assets/img/outer/languages/ruby-ss-1.png"
                         :ss-3 "/assets/img/outer/languages/ruby-ss-1.png"
-                        :testimonial-author-1 "/assets/img/outer/stories/john.jpg"
+                        :feature-1-title "Built For Ruby"
+                        :feature-1 "CircleCI uses RVM to provide support for a wide variety of Ruby versions and gems. It is also trivial to add any packages or frameworks that are not installed on our machines by default, allowing you to effortlessly customize your test enviroment.  CircleCI also supports Test::Unit, RSpec, Cucumber, Spinach, Jasmine, Konacha, and just about any other testing framework you use for your Ruby project."
+                        :feature-2-title "Database Management"
+                        :feature-2 "Circle manages all your database requirements for your, such as running your rake commands for creating, loading, and migrating your database. We have pre-installed more than a dozen databases and queues, including PostgreSQL, MySQL, and MongoDB. You can also add custom database commands via your circle.yml."
+                        :feature-3-title "Inference That Just Works"
+                        :feature-3 "For the majority of Ruby projects no configuration is required; you just run your builds on CircleCI and it works! CircleCI will automatically infer your test commands if you're using Test::Unit, RSpec, Cucumber, Spinach, Jasmine, or Konacha."
+                        :docs-link "/docs/language-ruby-on-rails"
                         :testimonials [{:text "Nullam id dolor id nibh ultricies vehicula ut id elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis."
                                         :img "/assets/img/outer/stories/john.jpg"
                                         :author "Kevin Rose"
@@ -67,17 +73,17 @@
                      [:img {:src "/assets/img/outer/languages/gear-icon.png"}]
                      ]
                     [:div.feature-copy
-                     [:h4.feature-title "Testing Frameworks"
+                     [:h4.feature-title (:feature-1-title template)
                       ]
-                     [:p.feature-description "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur."] 
+                     [:p.feature-description (:feature-1 template)] 
                      ]
                     
                     ]
                    [:div.feature
                     [:div.feature-copy
-                     [:h4.feature-title "Testing Frameworks"
+                     [:h4.feature-title (:feature-2-title template)
                       ]
-                     [:p.feature-description "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur."] 
+                     [:p.feature-description (:feature-2 template)] 
                      ]
                     [:div.feature-image
                      [:img {:src "/assets/img/outer/languages/book-icon.png"}]
@@ -88,14 +94,14 @@
                      [:img {:src "/assets/img/outer/languages/file-icon.png"}]
                      ]
                     [:div.feature-copy
-                     [:h4.feature-title "Testing Frameworks"
+                     [:h4.feature-title (:feature-3-title template)
                       ]
-                     [:p.feature-description "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur."] 
+                     [:p.feature-description (:feature-3 template)] 
                      ]
                     
                     ]
                    [:div.button 
-                    [:a {:href="#"} "Read documentation on " (:language template)]]
+                    [:a {:href (:docs-link template)} "Read documentation on " (:language template)]]
                    ]
                   ]
                  [:div.languages-testimonials {:class (arrow-class selected-testimonial)}
@@ -111,12 +117,8 @@
                     [:div.testimonial
                      [:p.testimonial-text (get-in template [:testimonials selected-testimonial :text])]
                      
-                     [:div.testimonial-author "- " (get-in template [:testimonials selected-testimonial :author])]
+                     [:div.testimonial-author "—" (get-in template [:testimonials selected-testimonial :author])]
                      [:div.testimonial-author-title (get-in template [:testimonials selected-testimonial :title])]]
-                    ;;[:div.testimonial
-                    ;;[:div.testimonial-text (:testimonial-2 template)]]
-                    ;;[:div.testimonial
-                    ;;[:div.testimonial-text (:testimonial-3 template)]]
                     ]
                    ]
                   ]
@@ -128,17 +130,32 @@
                    [:div.languages-cta-steps
                     [:div.languages-cta-step
                      [:div.step-number "1"]
-                      "Start by signing up using GitHub"
+                     [:div
+                      "Start by signing up "
+                      [:br] 
+                      "using GitHub"]
                      ]
-                     [:div.languages-cta-step
-                      [:div.step-number "2"]
-                      "Run one of your Ruby projects on Circle"]
-                     [:div.languages-cta-step
-                      [:div.step-number "3"]
-                      "That's it! If you hit any problems just us a message and we'll help you out."]
-                     ]
-                   [:a.languages-cta-button {:href="#"}
-                    "Sign Up With GitHub"]
+                    [:div.languages-cta-step
+                     [:div.step-number "2"]
+                     [:div 
+                      "Run one of your Ruby projects on Circle"]]
+                    [:div.languages-cta-step
+                     [:div.step-number "3"]
+                     [:div
+                      [:strong "That's it!"]
+                      " Contact support if you run in to any issues. "]
+                     ]]
+                   [:div.cta-divider]
+                   [:div.center-text
+                    [:a.languages-cta-button
+                     ;come back to this
+                     ;{:href (auth-url)
+                     ;:on-click #(put! controls-ch [:track-external-link-clicked {:event "Auth GitHub"
+                     ;                                                           :properties {:source (:language template) "page"}
+                     ;                                                          :path (auth-url)}])}
+                     "Sign Up With GitHub"]
+                    [:div.language-cta-trial "14-day free trial"]
+                    ]
                    ]]
                  
                  ])))))
