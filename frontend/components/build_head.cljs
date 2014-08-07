d2e3630bae70656fa9a29f83f2372f40da60c56a
Emile Snyder <emile@circleci.com>
2014-08-07 15:52:14 -0700

    Remove useless scope check, per review.

--- frontend/components/build_head.cljs
+++ frontend/components/build_head.cljs
@@ -301,52 +301,51 @@
                        :href (build-model/path-for-parallelism build)}
                    (str (:parallel build) "x")]
                   [:span (:parallel build) "x"])]]]]
-            (if (has-scope :view-builds data)
-              [:div.build-actions
-               [:div.actions
-                (forms/stateful-button
-                 [:button.retry_build
-                  {:data-loading-text "Rebuilding",
-                   :title "Retry the same tests",
-                   :on-click #(put! controls-ch [:retry-build-clicked {:build-id build-id
-                                                                       :vcs-url vcs-url
-                                                                       :build-num build-num
-                                                                       :clear-cache? false}])}
-                  "Rebuild"])
-                
-                (forms/stateful-button
-                 [:button.clear_cache_retry
-                  {:data-loading-text "Rebuilding",
-                   :title "Clear cache and retry",
-                   :on-click #(put! controls-ch [:retry-build-clicked {:build-id build-id
-                                                                       :vcs-url vcs-url
-                                                                       :build-num build-num
-                                                                       :clear-cache? true}])}
-                  "& clear cache"])
+            [:div.build-actions
+             [:div.actions
+              (forms/stateful-button
+               [:button.retry_build
+                {:data-loading-text "Rebuilding",
+                 :title "Retry the same tests",
+                 :on-click #(put! controls-ch [:retry-build-clicked {:build-id build-id
+                                                                     :vcs-url vcs-url
+                                                                     :build-num build-num
+                                                                     :clear-cache? false}])}
+                "Rebuild"])
+              
+              (forms/stateful-button
+               [:button.clear_cache_retry
+                {:data-loading-text "Rebuilding",
+                 :title "Clear cache and retry",
+                 :on-click #(put! controls-ch [:retry-build-clicked {:build-id build-id
+                                                                     :vcs-url vcs-url
+                                                                     :build-num build-num
+                                                                     :clear-cache? true}])}
+                "& clear cache"])
 
-                (if (has-scope :write-settings data)
-                  (forms/stateful-button
-                   [:button.ssh_build
-                    {:data-loading-text "Rebuilding",
-                     :title "Retry with SSH in VM",
-                     :on-click #(put! controls-ch [:ssh-build-clicked {:build-id build-id
-                                                                       :vcs-url vcs-url
-                                                                       :build-num build-num}])}
-                    "& enable ssh"]))]
-               [:div.actions
-                [:button.report_build
-                 {:title "Report error with build",
-                  :on-click #(put! controls-ch [:report-build-clicked {:build-url (:build_url @build)}])}
-                 "Report"]
-                (when (build-model/can-cancel? build)
-                  (forms/stateful-button
-                   [:button.cancel_build
-                    {:data-loading-text "Canceling",
-                     :title "Cancel this build",
-                     :on-click #(put! controls-ch [:cancel-build-clicked {:build-id build-id
-                                                                          :vcs-url vcs-url
-                                                                          :build-num build-num}])}
-                    "Cancel"]))]])
+              (if (has-scope :write-settings data)
+                (forms/stateful-button
+                 [:button.ssh_build
+                  {:data-loading-text "Rebuilding",
+                   :title "Retry with SSH in VM",
+                   :on-click #(put! controls-ch [:ssh-build-clicked {:build-id build-id
+                                                                     :vcs-url vcs-url
+                                                                     :build-num build-num}])}
+                  "& enable ssh"]))]
+             [:div.actions
+              [:button.report_build
+               {:title "Report error with build",
+                :on-click #(put! controls-ch [:report-build-clicked {:build-url (:build_url @build)}])}
+               "Report"]
+              (when (build-model/can-cancel? build)
+                (forms/stateful-button
+                 [:button.cancel_build
+                  {:data-loading-text "Canceling",
+                   :title "Cancel this build",
+                   :on-click #(put! controls-ch [:cancel-build-clicked {:build-id build-id
+                                                                        :vcs-url vcs-url
+                                                                        :build-num build-num}])}
+                  "Cancel"]))]]
             [:div.no-user-actions]]
            (when (:show-usage-queue usage-queue-data)
              (om/build build-queue {:build build
