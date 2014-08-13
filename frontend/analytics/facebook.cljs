6b38ee915530da48875e7e7d6c7f17f30ddcbef9
worldsoup <nick@circleci.com>
2014-08-13 09:46:12 -0700

    fix functions

--- frontend/analytics/facebook.cljs
+++ frontend/analytics/facebook.cljs
@@ -2,17 +2,19 @@
   (:require [frontend.utils :as utils :include-macros true]
             [goog.net.jsloader]))
 
+(defn track-conversion []
+  (.push js/window._fbq #js ["track" "6017231164176" {:value "0.01" :currency "USD"}]))
 
-(defn track-signup 
-   (if (aget js/window "_fbq")
-         (track-fb-conversion (.push js/window._fbq #js ["track" "6017231164176" {:value "0.01" :currency "USD"}]))
-         (-> (goog.net.jsloader.load "//connect.facebook.net/en_US/fbds.js")
-                     (.addCallback #(track-pid pid)))))
-
-
-
-
-
-
-
-
+(defn track-signup []
+  (if (aget js/window "_fbq")
+    (track-conversion)
+    (-> (goog.net.jsloader.load "//connect.facebook.net/en_US/fbds.js")
+        (.addCallback track-conversion)))
+  
+  
+  
+  
+  
+  
+  
+  
