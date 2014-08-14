271e8c607d72d3834cbbca034c761bd038a29ef1
Gordon Syme <gordonsyme@gmail.com>
2014-08-14 18:34:47 +0100

    Add deep-merge fn for merging nested maps.

--- frontend/utils.cljs
+++ frontend/utils.cljs
@@ -199,3 +199,47 @@
 
 (defn valid-email? [str]
   (.isValidAddrSpec EmailAddress str))
+
+(defn deep-merge* [& maps]
+  (let [f (fn [old new]
+            (if (and (map? old) (map? new))
+              (merge-with deep-merge* old new)
+              new))]
+    (if (every? map? maps)
+      (apply merge-with f maps)
+      (last maps))))
+
+(defn deep-merge
+  "Merge nested maps. At each level maps are merged left to right. When all
+  maps have a common key whose value is also a map, those maps are merged
+  recursively. If any of the values are not a map then the value from the
+  right-most map is chosen.
+
+  E.g.:
+  user=> (deep-merge {:a {:b 1}} {:a {:c 3}})
+  {:a {:c 3, :b 1}}
+
+  user=> (deep-merge {:a {:b 1}} {:a {:b 2}})
+  {:a {:b 2}}
+
+  user=> (deep-merge {:a {:b 1}} {:a {:b {:c 4}}})
+  {:a {:b {:c 4}}}
+
+  user=> (deep-merge {:a {:b {:c 1}}} {:a {:b {:e 2 :c 15} :f 3}})
+  {:a {:f 3, :b {:e 2, :c 15}}}
+
+  Each of the arguments to this fn must be maps:
+
+  user=> (deep-merge {:a 1} [1 2])
+  AssertionError Assert failed: (and (map? m) (every? map? ms))
+
+  Like merge, a key that maps to nil will override the same key in an earlier
+  map that maps to a non-nil value:
+
+  user=> (deep-merge {:a {:b {:c 1}, :d {:e 2}}}
+                     {:a {:b nil, :d {:f 3}}})
+  {:a {:b nil, :d {:f 3, :e 2}}}"
+  [& maps]
+  (let [maps (filter identity maps)]
+    (assert (every? map? maps))
+    (apply merge-with deep-merge* maps)))
