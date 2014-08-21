85e5b7bc4a0e1a3ce9a2cab64535710088302faf
worldsoup <nick@circleci.com>
2014-08-20 17:58:03 -0700

    add swallow-errors

--- frontend/analytics/facebook.cljs
+++ frontend/analytics/facebook.cljs
@@ -6,8 +6,9 @@
   (.push js/window._fbq #js ["track" "6017231164176" {:value "0.01" :currency "USD"}]))
 
 (defn track-signup []
-  (if (aget js/window "_fbq")
-    (track-conversion)
-    (-> (goog.net.jsloader.load "//connect.facebook.net/en_US/fbds.js")
-        (.addCallback track-conversion)))
+  (utils/swallow-errors
+    (if (aget js/window "_fbq")
+      (track-conversion)
+      (-> (goog.net.jsloader.load "//connect.facebook.net/en_US/fbds.js")
+          (.addCallback track-conversion))))
   
