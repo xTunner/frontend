453937b39ae4bf80d80af81cb51578a8fbd66f63
Daniel Woelfel <dwwoelfel@gmail.com>
2014-10-11 12:29:28 -0700

    flesh out component instrumentation

--- frontend/instrumentation.cljs
+++ frontend/instrumentation.cljs
@@ -1,7 +1,14 @@
 (ns frontend.instrumentation
-  (:require [frontend.state :as state]
+  (:require [cljs-time.core :as time]
+            [cljs-time.format :as time-format]
+            [dommy.core :as dommy]
+            [frontend.components.key-queue :as keyq]
+            [frontend.datetime :as datetime]
+            [frontend.state :as state]
             [frontend.utils :as utils :include-macros true]
-            [om.core :as om :include-macros true]))
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true])
+  (:use-macros [dommy.macros :only [node sel sel1]]))
 
 (defn wrap-api-instrumentation [handler api-data]
   (fn [state]
@@ -21,10 +28,103 @@
           (utils/merror e)
           state)))))
 
+(def component-stats (atom {}))
+
+(defn react-id [x]
+  (let [id (.-_rootNodeID x)]
+    (assert id)
+    id))
+
 (def instrumentation-methods
   (om/specify-state-methods!
-   (clj->js (update-in om/pure-methods [:componentWillUpdate] (fn [f]
-                                                                (fn [next-props next-state]
-                                                                  (this-as this
-                                                                           (js/console.log "updating" (.getDisplayName this))
-                                                                           (.call f this next-props next-state))))))))
+   (-> om/pure-methods
+       (update-in [:componentWillUpdate]
+                  (fn [f]
+                    (fn [next-props next-state]
+                      (this-as this
+                        (let [dname ((aget this "getDisplayName"))]
+                          (swap! component-stats update-in [(react-id this)] merge {:display-name dname
+                                                                                    :last-will-update (time/now)}))
+                        (.call f this next-props next-state)))))
+       (update-in [:componentDidUpdate]
+                  (fn [f]
+                    (fn [prev-props prev-state]
+                      (this-as this
+                        (swap! component-stats update-in [(react-id this)]
+                               (fn [stats]
+                                 (let [now (time/now)]
+                                   (-> stats
+                                       (assoc :last-did-update now)
+                                       (update-in [:render-ms] (fnil conj [])
+                                                  (if (time/after? now (:last-will-update stats))
+                                                    (time/in-millis (time/interval (:last-will-update stats) now))
+                                                    0))))))
+                        (.call f this prev-props prev-state)))))
+       (clj->js))))
+
+(defn avg [coll]
+  (/ (reduce + coll)
+     (count coll)))
+
+(defn std-dev [coll]
+  (let [a (avg coll)]
+    (Math/sqrt (avg (map #(Math/pow (- % a) 2) coll)))))
+
+(defn stats-view [data owner]
+  (om/component
+   (dom/figure nil
+     (om/build keyq/KeyboardHandler {}
+               {:opts {:keymap (atom {["ctrl+k"] #(om/transact! data (constantly {}))
+                                      ["ctrl+j"] #(om/update-state! owner :shown? not)})}})
+
+     (when (om/get-state owner :shown?)
+       (let [stats (map (fn [[display-name renders]]
+                          (let [times (mapcat :render-ms renders)]
+                            {:display-name (or display-name "Unknown")
+                             :render-count (count times)
+                             :last-will-update (last (sort (map :last-will-update renders)))
+                             :last-render-ms (last (:render-ms (last (sort-by :last-did-update renders))))
+                             :average-render-ms (when (seq times) (int (avg times)))
+                             :max-render-ms (when (seq times) (apply max times))
+                             :min-render-ms (when (seq times) (apply min times))
+                             :std-dev (when (seq times) (int (std-dev times)))}))
+                        (reduce (fn [acc [react-id data]]
+                                  (update-in acc [(:display-name data)] (fnil conj []) data))
+                                {} data))]
+         (dom/div #js {:className "admin-stats"}
+           (dom/table nil
+             (dom/caption nil "Component render stats, sorted by last update. Clicks go right through like it's not there. Ctrl+j to toggle, Ctrl+k to clear.")
+             (dom/thead nil
+               (dom/tr nil
+                 (dom/th nil "component")
+                 (dom/th #js {:className "number"} "count")
+                 (dom/th #js {:className "number"} "last-update")
+                 (dom/th #js {:className "number"} "last-ms")
+                 (dom/th #js {:className "number"} "average-ms")
+                 (dom/th #js {:className "number"} "max-ms")
+                 (dom/th #js {:className "number"} "min-ms")
+                 (dom/th #js {:className "number"} "std-ms")))
+             (apply dom/tbody nil
+                    (for [{:keys [display-name last-will-update average-render-ms
+                                  max-render-ms min-render-ms std-dev render-count
+                                  last-render-ms] :as stat} (reverse (sort-by :last-will-update stats))]
+                      (dom/tr nil
+                        (dom/td nil display-name)
+                        (dom/td #js {:className "number" } render-count)
+                        (dom/td #js {:className "number" }
+                          (when last-will-update
+                            (time-format/unparse (time-format/formatters :hour-minute-second)
+                                                 last-will-update)))
+                        (dom/td #js {:className "number" } last-render-ms)
+                        (dom/td #js {:className "number" } average-render-ms)
+                        (dom/td #js {:className "number" } max-render-ms)
+                        (dom/td #js {:className "number" } min-render-ms)
+                        (dom/td #js {:className "number" } std-dev)))))))))))
+
+(defn setup-component-stats! []
+  (let [stats-node (node [:div.om-instrumentation])]
+    (dommy/append! (sel1 :body) stats-node)
+    (om/root
+     stats-view
+     component-stats
+     {:target stats-node})))
