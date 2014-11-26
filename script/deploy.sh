a18c57d8fc18e16868229cd6f29e29351b84f7a5
Brandon Bloom <brandon@brandonbloom.name>
2014-11-26 16:00:48 -0500

    auto-create backend branch

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,19 +7,20 @@ dist_tgz=$DEPLOY_S3_BUCKET/dist/$CIRCLE_SHA1.tgz
 tar -cz resources/public/ | aws put --http $dist_tgz
 echo $CIRCLE_SHA1 | aws put --http $DEPLOY_S3_BUCKET/branch/$CIRCLE_BRANCH
 
-# Check for matching branch name on backend.
+# Check for matching branch name on backend or create one from production branch.
 export GIT_SSH='./script/git-ssh-wrap.sh'
 export KEYPATH="$HOME/.ssh/id_frontend-private"
-backend_heads=$(git ls-remote --heads git@github.com:circleci/circle)
-if echo $backend_heads | grep "refs/heads/$CIRCLE_BRANCH$" ; then
-  backend_branch=$CIRCLE_BRANCH
-else
-  backend_branch=production
+backend_repo=git@github.com:circleci/circle
+backend_heads=$(git ls-remote --heads $backend_repo)
+if ! echo $backend_heads | grep "refs/heads/$CIRCLE_BRANCH$" ; then
+  backend_dir=$HOME/checkouts/circle
+  git clone $backend_repo $backend_dir
+  git -C $backend_dir push origin origin/production:$CIRCLE_BRANCH
 fi
 
 # Trigger a backend build of this sha1.
 circle_api=https://circleci.com/api/v1
-tree_url=$circle_api/project/circleci/circle/tree/$backend_branch
+tree_url=$circle_api/project/circleci/circle/tree/$CIRCLE_BRANCH
 http_status=$(curl -o /dev/null \
                    --silent \
                    --write-out '%{http_code}\n' \
