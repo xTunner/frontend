20b29785abce299d349a4d5b33424897a7932021
Travis Vachon <travis.vachon@gmail.com>
2014-11-25 17:42:24 -0800

    Track last-touch UTM params in mixpanel

--- frontend/core.cljs
+++ frontend/core.cljs
@@ -122,7 +122,7 @@
    (analytics/track-message (first value))))
 
 (defn nav-handler
-  [[navigation-point {:keys [inner?] {:keys [join]} :query-params :as args} :as value] state history]
+  [[navigation-point {:keys [inner?] {:keys [join] :as query-params} :query-params :as args} :as value] state history]
   (when (log-channels?)
     (mlog "Navigation Verbose: " value))
   (swallow-errors
@@ -130,6 +130,7 @@
      (let [previous-state @state]
        (swap! state (partial nav-con/navigated-to history navigation-point args))
        (nav-con/post-navigated-to! history navigation-point args previous-state @state)
+       (analytics/register-last-touch-utm query-params)
        (when join (analytics/track-join-code join))
        (analytics/track-view-page (if inner? :inner :outer))))))
 
@@ -322,4 +323,3 @@
   (fw/watch-and-reload
     :websocket-url "ws://localhost:3449/figwheel-ws"
     :jsload-callback (fn [] (update-ui!))))
-
