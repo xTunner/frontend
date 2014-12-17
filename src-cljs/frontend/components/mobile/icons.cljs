4c381edeb567d7985ebe9ee7e55e86c68a193123
Travis Vachon <travis.vachon@gmail.com>
2014-12-17 02:45:03 -0500

    Use cdn for images

--- src-cljs/frontend/components/mobile/icons.cljs
+++ src-cljs/frontend/components/mobile/icons.cljs
@@ -11,45 +11,45 @@
   (:require-macros [frontend.utils :refer [defrender html]]))
 
 (def app-store
-  (html [:img.app-store {:src "/img/outer/mobile/app-store.svg"
+  (html [:img.app-store {:src (utils/cdn-path "/img/outer/mobile/app-store.svg")
                          :alt "Improve App Store Rating"}]))
 
 (def build-env
-  (html [:img.build-env {:src "/img/outer/mobile/build-envs.svg"
+  (html [:img.build-env {:src (utils/cdn-path "/img/outer/mobile/build-envs.svg")
                          :alt "Control your mobile app testing environment"}]))
 
 (def commit
-  (html [:img.commit {:src "/img/outer/mobile/commit.svg"
+  (html [:img.commit {:src (utils/cdn-path "/img/outer/mobile/commit.svg")
                       :alt "Merge App code with confidence"}]))
 
 (def deploy
-  (html [:img.deploy {:src "/img/outer/mobile/deploy.svg"
+  (html [:img.deploy {:src (utils/cdn-path "/img/outer/mobile/deploy.svg")
                       :alt "Automate Mobile Continuous Deployment"}]))
 
 (def htc
-  (html [:img.htc {:src "/img/outer/mobile/htc.svg"
+  (html [:img.htc {:src (utils/cdn-path "/img/outer/mobile/htc.svg")
                    :alt "Mobile Android app testing"}]))
 
 (def iphone
-  (html [:img.iphone {:src "/img/outer/mobile/iphone.svg"
+  (html [:img.iphone {:src (utils/cdn-path "/img/outer/mobile/iphone.svg")
                       :alt "Mobile iPhone app testing"}]))
 
 (def nexus
-  (html [:img.nexus {:src "/img/outer/mobile/nexus.svg"
+  (html [:img.nexus {:src (utils/cdn-path "/img/outer/mobile/nexus.svg")
                      :alt "Mobile Android app testing"}]))
 
 (def setup
-  (html [:img.setup {:src "/img/outer/mobile/setup.svg"
+  (html [:img.setup {:src (utils/cdn-path "/img/outer/mobile/setup.svg")
                      :alt "Easy Continuous Integration setup"}]))
 
 (def steps
-  (html [:img.steps {:src "/img/outer/mobile/steps.svg"
+  (html [:img.steps {:src (utils/cdn-path "/img/outer/mobile/steps.svg")
                      :alt "Mobile Continuous Integration and Deployment"}]))
 
 (def testing
-  (html [:img.testing {:src "/img/outer/mobile/testing.svg"
+  (html [:img.testing {:src (utils/cdn-path "/img/outer/mobile/testing.svg")
                        :alt "Automate App Testing"}]))
 
 (def workflow
-  (html [:img.workflow {:src "/img/outer/mobile/workflow.svg"
+  (html [:img.workflow {:src (utils/cdn-path "/img/outer/mobile/workflow.svg")
                         :alt "Mobile Continuous Integration Workflow"}]))
