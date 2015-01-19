9ff501cc2c23a297a94dc477778674aedece25db
Brandon Bloom <brandon@brandonbloom.name>
2015-01-19 14:22:26 -0500

    env and docstrings in config

--- src-cljs/frontend/config.cljs
+++ src-cljs/frontend/config.cljs
@@ -1,7 +1,17 @@
 (ns frontend.config)
 
-(defn enterprise? []
+(defn env
+  "The name of the server configuration environment.
+  For humans only: Do not gate features with this setting."
+  []
+  (aget js/window "renderContext" "env"))
+
+(defn enterprise?
+  "True if this is an enterprise (as opposed to main public web) deployment."
+  []
   (boolean (aget js/window "renderContext" "enterprise")))
 
-(defn pusher []
+(defn pusher
+  "Options to be passed to the Pusher client library."
+  []
   (js->clj (aget js/window "renderContext" "pusher")))
