e8ff6de3ef70440164860b492c4d0e1ee2c15faf
Ian Duncan <ian@iankduncan.com>
2015-03-09 16:30:49 -0400

    Remove old unused pricing page

--- src-cljs/frontend/components/app.cljs
+++ src-cljs/frontend/components/app.cljs
@@ -22,8 +22,7 @@
             [frontend.components.jobs :as jobs]
             [frontend.components.key-queue :as keyq]
             [frontend.components.placeholder :as placeholder]
-            [frontend.components.pricing :as pricing]
-            [frontend.components.pricing-b :as pricing-b]
+            [frontend.components.pricing-b :as pricing]
             [frontend.components.privacy :as privacy]
             [frontend.components.project-settings :as project-settings]
             [frontend.components.security :as security]
@@ -67,8 +66,7 @@
 
     :landing landing/home
     :about about/about
-    ; :pricing pricing/pricing
-    :pricing pricing-b/pricing
+    :pricing pricing/pricing
     :jobs jobs/jobs
     :privacy privacy/privacy
     :security security/security
