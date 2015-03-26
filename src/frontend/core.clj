3135184ceb0d39ea29e67f3d764582d9423799bb
Kevin Bell <kevin.a.bell@gmail.com>
2015-03-25 22:47:35 -0700

    Contact to its own route

--- src/frontend/core.clj
+++ src/frontend/core.clj
@@ -78,6 +78,7 @@
               ;; the backend to serve HTML to bootstrap the client-side code.
               #"/"
               #"/about"
+              #"/contact"
               #"/account.*"
               #"/add-projects"
               #"/changelog.*"
