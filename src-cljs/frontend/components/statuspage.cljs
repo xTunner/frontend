80dd1777ece54957643b3c350e9e7c4305360681
Marc O'Morain <marc@circleci.com>
2015-07-12 08:59:33 +0100

    More clear status messages during outages

--- src-cljs/frontend/components/statuspage.cljs
+++ src-cljs/frontend/components/statuspage.cljs
@@ -13,27 +13,54 @@
 (def page prod-page)
 (def page-url (str "https://" page ".statuspage.io/api/v2/summary.json"))
 
-;; example data from https://manage.statuspage.io/pages/6w4r0ttlx5ft/status-widget
-(def example-summary-json
-  {:page {:id "6w4r0ttlx5ft", :name "CircleCI", :url "http://status.circleci.com", :updated_at "2015-05-13T01:01:48-07:00"}, :status {:description "Partial System Outage", :indicator "major"}, :components [{:created_at "2014-05-03T01:22:07.274Z", :description nil, :id "b13yz5g2cw10", :name "API", :page_id "6w4r0ttlx5ft", :position 1, :status "partial_outage", :updated_at "2014-05-14T20:34:43.340Z"} {:created_at "2014-05-03T01:22:07.286Z", :description nil, :id "9397cnvk62zn", :name "Management Portal", :page_id "6w4r0ttlx5ft", :position 2, :status "major_outage", :updated_at "2014-05-14T20:34:44.470Z"}], :incidents [{:page_id "6w4r0ttlx5ft", :incident_updates [{:body "Our master database has ham sandwiches flying out of the rack, and we're working our hardest to stop the bleeding. The whole site is down while we restore functionality, and we'll provide another update within 30 minutes.", :created_at "2014-05-14T14:22:40.301-06:00", :display_at "2014-05-14T14:22:40.301-06:00", :id "jdy3tw5mt5r5", :incident_id "cp306tmzcl0y", :status "identified", :updated_at "2014-05-14T14:22:40.301-06:00"}], :resolved_at nil, :name "Unplanned Database Outage", :shortlink "http://stspg.dev:5000/Q0E", :updated_at "2014-05-14T14:35:21.711-06:00", :status "identified", :id "cp306tmzcl0y", :monitoring_at nil, :impact "critical", :created_at "2014-05-14T14:22:39.441-06:00"}], :scheduled_maintenances [{:page_id "6w4r0ttlx5ft", :incident_updates [{:body "Our data center has informed us that they will be performing routine network maintenance. No interruption in service is expected. Any issues during this maintenance should be directed to our support center", :created_at "2014-05-14T14:24:41.913-06:00", :display_at "2014-05-14T14:24:41.913-06:00", :id "qq0vx910b3qj", :incident_id "w1zdr745wmfy", :status "scheduled", :updated_at "2014-05-14T14:24:41.913-06:00"}], :resolved_at nil, :name "Network Maintenance (No Interruption Expected)", :scheduled_for "2014-05-17T22:00:00.000-06:00", :shortlink "http://stspg.dev:5000/Q0F", :scheduled_until "2014-05-17T23:30:00.000-06:00", :updated_at "2014-05-14T14:24:41.918-06:00", :status "scheduled", :id "w1zdr745wmfy", :monitoring_at nil, :impact "none", :created_at "2014-05-14T14:24:40.430-06:00"} {:page_id "6w4r0ttlx5ft", :incident_updates [{:body "Scheduled maintenance is currently in progress. We will provide updates as necessary.", :created_at "2014-05-14T14:34:20.036-06:00", :display_at "2014-05-14T14:34:20.036-06:00", :id "drs62w8df6fs", :incident_id "k7mf5z1gz05c", :status "in_progress", :updated_at "2014-05-14T14:34:20.036-06:00"} {:body "We will be performing rolling upgrades to our web tier with a new kernel version so that Heartbleed will stop making us lose sleep at night. Increased load and latency is expected, but the app should still function appropriately. We will provide updates every 30 minutes with progress of the reboots.", :created_at "2014-05-14T14:27:18.845-06:00", :display_at "2014-05-14T14:27:18.845-06:00", :id "z40y7398jqxc", :incident_id "k7mf5z1gz05c", :status "scheduled", :updated_at "2014-05-14T14:27:18.845-06:00"}], :resolved_at nil, :name "Web Tier Recycle", :scheduled_for "2014-05-14T14:30:00.000-06:00", :shortlink "http://stspg.dev:5000/Q0G", :scheduled_until "2014-05-14T16:30:00.000-06:00", :updated_at "2014-05-14T14:35:12.258-06:00", :status "in_progress", :id "k7mf5z1gz05c", :monitoring_at nil, :impact "minor", :created_at "2014-05-14T14:27:17.303-06:00"}]})
+(comment def sample-status
+;; There are 2 methods to set sample statuses when testing the statuspage.io
+;; widget.
+;; - The most simple is to (def page test-page) and log in to
+;;   statuspage.io and change the "CircleCI Test" page status as required.
+;; -  The second method is to load one of the sample JSON files listed below
+;;   and (def sample-status) to the loaded document. When loading, the JSON
+;;   keys should be converted to Cloure keywords, using something like this:
+;;   (clojure.data.json/read (clojure.java.io/reader file-name :key-fn keyword))
+;; The om state for [:statuspage :summary] should then be set as in the comment
+;; in update-statuspage-state
+;;
+;; The following test data is commited to the repo:
+;; test/json/statuspage.io/github-degraded.json
+;; test/json/statuspage.io/github-preventing-builds.json
+;; test/json/statuspage.io/incident-and-mutliple-components.json
+;; test/json/statuspage.io/normal.json
+  {})
 
 (defn update-statuspage-state [component]
-  ;;(om/set-state! component :statuspage {:summary example-summary-json})
+  (comment om/set-state! component :statuspage {:summary sample-status})
   (go
    (let [res (<! (ajax/managed-ajax :get page-url))]
      (when (= :success (:status res))
        (om/set-state! component :statuspage {:summary (->> res :resp)})))))
 
 (defn incident-markup [incident]
-  (if-let [updates (:incident_updates incident)]
-    (html [:p
-           [:a {:href (str "https://" page ".statuspage.io")}
-            (str (:impact incident) " incident")]
-           (str " created at " (:created_at incident) " - updated at " (:updated_at (first updates)))
-           [:br]
-           (str (:body (first updates)))])
-    (html [:p
-           (str (:impact incident) " incident created at " (:created_at incident))])))
+  (let [status (first (:incident_updates incident))]
+    (html
+      [:a {:href (:shortlink incident)}
+       [:h4 (:name incident)]
+       [:p (:body status)
+        [:br]
+        "Updated " (datetime/as-time-since (:updated_at status))]])))
+
+(defn severity-class
+  "Given the statuspage data, return one of none, minor, major or critical.
+  The statuspage.io API has 2 places that we need to check to get an indication
+  of the system health.
+  First, there is a global status at :status :indicator
+  Seconly, there is an array of active incidents, each of which has a :impact."
+  [response]
+  ;; Build a set of the available statuses.
+  (let [severities (apply hash-set (get-in response [:status :indicator])
+                                   (map :impact (:incidents response)))]
+    ;; Find the first severity in the vector below that exists in severities
+    (or (some severities ["critical" "major" "minor"])
+        "none")))
 
 (defn statuspage [app owner]
   (reify
@@ -54,12 +81,14 @@
     om/IRender
     (render [_]
       (let [summary-response (:summary (om/get-state owner :statuspage))
-            ind (get-in summary-response [:status :indicator]) ;; none, minor, major or critical
             desc (get-in summary-response [:status :description])
-            class (or ind "none")]
+            components-affected (filter (comp not (partial = "operational") :status)
+                                        (:components summary-response))
+            class (severity-class summary-response)]
         (html [:div#statuspage-bar {:class class}
-               [:h4 (str desc " - components affected: "
-                         (clojure.string/join ", " (map #(str (:name %) " (" (:status %) ")")
-                                                        (:components summary-response))))]
                [:div (for [incident (:incidents summary-response)]
-                       (incident-markup incident))]])))))
+                       (incident-markup incident))]
+               (when (seq components-affected)
+                 [:a {:href (get-in summary-response [:page :url])}
+                  [:h4 (str desc " - components affected: "
+                            (clojure.string/join ", " (map (comp str :name) components-affected)))]])])))))
