277144b914fa3e69315f90e10b078d339a17e425
Kim, Hirokuni <yangkookkim@gmail.com>
2015-07-16 17:43:56 +0900

    Fix the alignment of login/sign-up buttons

--- src-cljs/frontend/components/header.cljs
+++ src-cljs/frontend/components/header.cljs
@@ -204,11 +204,12 @@
                                       :on-click #(raise! owner [:track-external-link-clicked {:path (auth-url) :event "login_click" :properties {:source "header log-in" :url js/window.location.pathname}}])
                                       :title "Log In with Github"}
                  "Log In"]]
-               [:li
-                [:button.login-link.btn.btn-success {:href (auth-url)
-                                                     :on-click #(raise! owner [:track-external-link-clicked {:path (auth-url) :event "signup_click" :properties {:source "header sign-up" :url js/window.location.pathname}}])
-                                                     :title "Sign up with Github"}
-                 "Sign Up"]]])]]
+               (list
+                 [:li
+                  [:button.login-link.btn.btn-success {:href (auth-url)
+                                                       :on-click #(raise! owner [:track-external-link-clicked {:path (auth-url) :event "signup_click" :properties {:source "header sign-up" :url js/window.location.pathname}}])
+                                                       :title "Sign up with Github"}
+                   "Sign Up"]])])]]
           (outer-subheader
            [{:mobile {:path "/mobile"
                        :title "Mobile"}
