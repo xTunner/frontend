e591c980f5d61c4a8cb4a18e14c403607fa592ca
Tim Reinke <tim.reinke@circleci.com>
2015-07-30 12:38:13 -0700

    Revert "Fixup stray parens"
    This reverts commit c9d666f6b982a7020510447024475dde613837f2.

--- src-cljs/frontend/components/build_head.cljs
+++ src-cljs/frontend/components/build_head.cljs
@@ -736,7 +736,9 @@
                          (map (fn [url] [:a {:href url} "#"
                                          (let [n (re-find #"/\d+$" url)]
                                            (if n (subs n 1) "?"))])
-                              urls))]))]
+                              urls))]))
+               ]
+
 
             [:tr
              [:th "Author"]
@@ -750,7 +752,10 @@
                         (if-not (:committer_email build)
                           [:span (build-model/committer build)]
                           [:a {:href (str "mailto:" (:committer_email build))}
-                           (build-model/committer build)])]))]]]
+                           (build-model/committer build)])]))]
+
+              ]]
+
 
             [:div.build-actions
              (when (has-scope :write-settings data)
