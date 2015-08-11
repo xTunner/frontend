9412c15e715ba4590c1fd321196f2f5d20475052
Travis Vachon <travis.vachon@gmail.com>
2015-08-10 18:12:24 -0700

    Add content for signup page
    Chatted with Kunal today - we're not going to bother adding the existing
    design to this page to A/B test since we have tons of data on how the
    current design converts. If this doesn't convert as well as historical
    data, we'll revert.

--- src-cljs/frontend/components/signup.cljs
+++ src-cljs/frontend/components/signup.cljs
@@ -1,6 +1,9 @@
 (ns frontend.components.signup
-  (:require [frontend.utils.github :as gh-util]
+  (:require [frontend.components.common :as common]
+            [frontend.utils.github :as gh-util]
+            [frontend.utils :as utils :include-macros true]
             [frontend.async :refer [raise!]]
+            [frontend.stefon :as stefon]
             [om.core :as om :include-macros true])
   (:require-macros [frontend.utils :refer [html]]))
 
@@ -10,11 +13,27 @@
     (render [_]
       (html
        [:section
-        [:div#signup
-         [:h1 "Authorize with GitHub first."]
-         [:h3 "Signup up using your GitHub login"]
-         [:a.btn.btn-cta {:href (gh-util/auth-url :destination "/")
-                          :on-click #(raise! owner [:track-external-link-clicked
-                                                    {:event "signup_click"
-                                                     :path (gh-util/auth-url :destination "/")}])}
-          "Authorize with GitHub"]]]))))
+        [:div#signup.container-fluid
+         [:div.row
+          [:div.col-md-1]
+          [:div.authorize-instructions.col-md-5
+           [:h1 "Authorize with GitHub first."]
+           [:p.github-signup-exp "Signup up using your GitHub login helps us start really fast."]
+           [:h5 (common/ico :check-icon) "Access to email address"]
+           [:h5 (common/ico :check-icon) "Read and write access to public repository"]
+           [:h5 (common/ico :check-icon) "Read and write access to private repository"]
+           [:a.btn.btn-cta.authorize-button
+            {:href (gh-util/auth-url :destination "/")
+             :on-click #(raise! owner [:track-external-link-clicked
+                                       {:event "signup_click"
+                                        :path (gh-util/auth-url :destination "/")}])}
+            (common/ico :github) "Authorize with GitHub"]]
+          [:div.github-example.col-md-6
+           [:img {:src (stefon/asset-path "/img/outer/signup/github-example.png")}]
+           [:p "What you'll see next"]]]
+         [:div.row.footer
+          [:div.github-example.col-md-12
+           [:a {:href "/privacy"} "Privacy"]
+           [:a {:href "/contact"} "Contact Us"]
+           [:a {:href "/"} "© CircleCI"]]]
+         ]]))))
