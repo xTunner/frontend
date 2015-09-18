f24dfc0e1375e3c395f3a83ef3dcc5033eee3b72
Peter Jaros <peter.a.jaros@gmail.com>
2015-09-18 17:35:04 -0400

    Show actual days left in trial.

--- src-cljs/frontend/components/license.cljs
+++ src-cljs/frontend/components/license.cljs
@@ -1,5 +1,7 @@
 (ns frontend.components.license
-  (:require [om.core :as om :include-macros true])
+  (:require [om.core :as om :include-macros true]
+            [cljs-time.coerce :as t-coerce]
+            [cljs-time.core :as t])
   (:require-macros [frontend.utils :refer [html]]))
 
 (def show-banner?
@@ -10,10 +12,12 @@
     om/IDisplayName (display-name [_] "License Banner")
     om/IRender
     (render [_]
-      (html [:.license-banner
-             [:.indicator "Trial Account"]
-             [:.message "Welcome to CircleCI."]
-             [:.timing [:span.days "30"] " days left in trial"]
-             [:a.contact-sales
-              {:href "mailto:enterprise@circleci.com"}
-              "Contact Sales…"]]))))
+      (let [expiry-date (t-coerce/from-string (:expiry_date license))
+            days-until-expiry (t/in-days (t/interval (t/now) expiry-date))]
+        (html [:.license-banner
+               [:.indicator "Trial Account"]
+               [:.message "Welcome to CircleCI."]
+               [:.timing [:span.days days-until-expiry] " days left in trial"]
+               [:a.contact-sales
+                {:href "mailto:enterprise@circleci.com"}
+                "Contact Sales…"]])))))
