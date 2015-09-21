32cca3eb2cd77f108d5792abd4d529dde2271224
Peter Jaros <peter.a.jaros@gmail.com>
2015-09-21 11:56:15 -0400

    Hide license banner when we have nothing to show.

--- src-cljs/frontend/components/license.cljs
+++ src-cljs/frontend/components/license.cljs
@@ -4,9 +4,6 @@
             [cljs-time.core :as t])
   (:require-macros [frontend.utils :refer [html]]))
 
-(def show-banner?
-  (complement nil?))
-
 (defn- days-until-api-time
   "Takes a string representing a time in the format the API delivers it and
   returns the number of days until now."
@@ -16,29 +13,40 @@
        (t/interval (t/now))
        t/in-days))
 
+(defn- banner-for-license
+  "Returns the banner for the given license, or nil if no banner applies."
+  [license]
+  (case (:status license)
+    "current"
+    (let [days-until-expiry (days-until-api-time (:expiry_date license))]
+      [:.license-banner {:class (:status license)}
+       [:.indicator "Trial Account"]
+       [:.message "Welcome to CircleCI."]
+       [:.timing [:span.days days-until-expiry] " days left in trial"]
+       [:a.contact-sales
+        {:href "mailto:enterprise@circleci.com"}
+        "Contact Sales…"]])
+
+    "in-violation"
+    (let [days-until-suspension (days-until-api-time (:hard_expiry_date license))]
+      [:.license-banner {:class (:status license)}
+       [:.indicator "Trial Expired"]
+       [:.message "Your trial period has expired."]
+       [:.timing [:span.days days-until-suspension] " days before suspension"]
+       [:a.contact-sales
+        {:href "mailto:enterprise@circleci.com"}
+        "Contact Sales…"]])
+    nil))
+
+(defn show-banner?
+  "Should we show a banner for the given license?"
+  [license]
+  (boolean (banner-for-license license)))
+
 (defn license-banner [license owner]
   (reify
     om/IDisplayName (display-name [_] "License Banner")
     om/IRender
     (render [_]
       (html
-       (case (:status license)
-         "current"
-         (let [days-until-expiry (days-until-api-time (:expiry_date license))]
-           [:.license-banner {:class (:status license)}
-            [:.indicator "Trial Account"]
-            [:.message "Welcome to CircleCI."]
-            [:.timing [:span.days days-until-expiry] " days left in trial"]
-            [:a.contact-sales
-             {:href "mailto:enterprise@circleci.com"}
-             "Contact Sales…"]])
-
-         "in-violation"
-         (let [days-until-suspension (days-until-api-time (:hard_expiry_date license))]
-           [:.license-banner {:class (:status license)}
-            [:.indicator "Trial Expired"]
-            [:.message "Your trial period has expired."]
-            [:.timing [:span.days days-until-suspension] " days before suspension"]
-            [:a.contact-sales
-             {:href "mailto:enterprise@circleci.com"}
-             "Contact Sales…"]]))))))
+       (banner-for-license license)))))
