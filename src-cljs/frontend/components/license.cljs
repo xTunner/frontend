4993568b1658c88baec17a6a73a35fe0f62f0109
Peter Jaros <peter.a.jaros@gmail.com>
2015-09-21 15:09:54 -0400

    Handle pluralization (and make stat more generic).

--- src-cljs/frontend/components/license.cljs
+++ src-cljs/frontend/components/license.cljs
@@ -14,13 +14,25 @@
        t/in-days))
 
 (defn- banner-contents
-  ([indicator-text message] (banner-contents indicator-text message nil nil))
-  ([indicator-text message days days-description]
+  "Returns the contents of a license banner.
+  * indicator-text - Text to go in the little indicator pill.
+  * message - The main message in the banner.
+  * stat (optional) - A stat to inform the user of their usage. A map including:
+    * :count - The number to show.
+    * :nouns - A pair of [singular plural] versions of the noun to follow the count.
+    * :description - Further description to follow the noun."
+  ([indicator-text message] (banner-contents indicator-text message nil))
+  ([indicator-text message stat]
    (list
     [:.indicator indicator-text]
     [:.message message]
-    (when days
-      [:.timing [:span.days days] " " days-description])
+    (when-let [{[singular plural] :nouns :keys [count description]} stat]
+      [:.stat
+       [:span.count count]
+       " "
+       (if (= 1 count) singular plural)
+       " "
+       description])
     [:a.contact-sales
      {:href "mailto:enterprise@circleci.com"}
      "Contact Sales…"])))
@@ -34,14 +46,15 @@
           ["trial" "current"]
           (banner-contents "Trial Account"
                            "Welcome to CircleCI."
-                           (days-until-api-time (:expiry_date license))
-                           "days left in trial")
-
+                           {:count (days-until-api-time (:expiry_date license))
+                            :nouns ["day" "days"]
+                            :description "left in trial"})
           ["trial" "in-violation"]
           (banner-contents "Trial Expired"
                            "Your trial period has expired."
-                           (days-until-api-time (:hard_expiry_date license))
-                           "days before suspension")
+                           {:count (days-until-api-time (:hard_expiry_date license))
+                            :nouns ["day" "days"]
+                            :description "before suspension"})
           ["trial" "expired"]
           (banner-contents "Trial Ended"
                            "Your builds have been suspended. Contact Sales to reactivate your builds.")
