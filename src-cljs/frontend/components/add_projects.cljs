e055858a5327ba31c7bbdd968f2e0f7d7efc7dee
Frank Wang <frank.wangtao@gmail.com>
2015-09-29 13:52:27 -0400

    contact-repo-admin-tooltip Remove exclamation mark

--- src-cljs/frontend/components/add_projects.cljs
+++ src-cljs/frontend/components/add_projects.cljs
@@ -158,7 +158,7 @@
                  (when (:fork repo)
                    [:span.forked (str " (" (vcs-url/org-name (:vcs_url repo)) ")")])]
                 [:button {:on-click #(utils/open-modal "#inviteForm-addprojects")
-                          :title "You must be an admin to add a project on CircleCI!"}
+                          :title "You must be an admin to add a project on CircleCI"}
                  [:i.fa.fa-lock]
                  "Contact repo admin"]]))))))
 
