7b2effde86e213ee85add51ed3011c0dbf17728c
Justin Cowperthwaite <justinc474@gmail.com>
2015-09-29 17:53:21 -0700

    Updated the 401, 404, and 500 error pages to be the new design, for aesthetics

--- src-cljs/frontend/components/errors.cljs
+++ src-cljs/frontend/components/errors.cljs
@@ -1,5 +1,6 @@
 (ns frontend.components.errors
   (:require [frontend.state :as state]
+            [frontend.analytics :as analytics]
             [frontend.async :refer [raise!]]
             [frontend.components.common :as common]
             [frontend.utils :as utils :include-macros true]
@@ -17,35 +18,34 @@
             _ (utils/mlog "error-page render with orig-nav-point " orig-nav-point " and logged-in? " (boolean logged-in?))
             maybe-login-page? (some #{orig-nav-point} [:dashboard :build])]
         (html
-         [:div.page.error
-          [:div.banner
-           [:div.container
-            [:h1 status]
-            [:h3 (str (condp = status
-                        401 "Login required"
-                        404 "Page not found"
-                        500 "Internal server error"
-                        "Something unexpected happened"))]]]
-          [:div.container
-           (condp = status
-             401 [:p
-                  [:b [:a {:href (gh-utils/auth-url)
-                           :on-click #(raise! owner [:track-external-link-clicked
-                                                     {:event "login_click"
-                                                      :properties {:source "401"
-                                                                   :url js/window.location.pathname}
-                                                      :path (gh-utils/auth-url)}])}
-                       "Log in"]]
-                  " here to view this page"]
-             404 (if (and (not logged-in?) maybe-login-page?)
-                   [:div
-                    [:p "We're sorry; either that page doesn't exist or you need to be logged in to view it."]
-                    [:p [:b [:a {:href (gh-utils/auth-url)
-                                 :on-click #(raise! owner [:track-external-link-clicked
-                                                           {:event "login_click"
-                                                            :properties {:source "404"
-                                                                         :url js/window.location.pathname}
-                                                            :path (gh-utils/auth-url)}])} "Log in"] " here to view this page with your GitHub permissions."]]]
-                   [:p "We're sorry, but that page doesn't exist."])
-             500 [:p "We're sorry, but something broke"]
-             "Something completely unexpected happened")]])))))
+          [:div.page.error
+           [:div.jumbotron
+            common/language-background-jumbotron
+            [:div.banner
+             [:div.container
+              (condp = status 
+                401 [:h1 4 [:span.error-zero] 1]
+                404 [:h1 4 [:span.error-zero] 4]
+                500 [:h1 5 [:span.error-zero] 0]
+                [:span.error-zero])]]
+            [:div.container
+             [:p "Something doesn't look right ..."]
+             (cond
+               (= status 401) [:p.error-message "You'll need to "
+                    [:a {:href (gh-utils/auth-url)
+                         :on-click #(raise! owner [:track-external-link-clicked
+                                                   {:event "login_click"
+                                                    :properties {:source "401"
+                                                                 :url js/window.location.pathname}
+                                                    :path (gh-utils/auth-url)}])}
+                     "login"]
+                    " or " 
+                    [:a {:href "/signup"
+                         :on-mouse-up #(analytics/track-signup-click)}
+                     " signup" ] ]
+               (some #{status} '(404 500)) [:p.error-message "Try heading back to our "
+                          [:a {:href "/"} "homepage"]
+                          " or checking out our "
+                          [:a {:href "http://blog.circleci.com/"} "blog"]]
+               :else "Something completely unexpected happened"
+               )]]])))))
