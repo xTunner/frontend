ff80116e342813d9a8c99ab428a5b5125420488d
Brady Lill <bradylill@gmail.com>
2015-10-21 17:12:43 -0700

    Fixing scroll to bottom on ui v2.

--- src-cljs/frontend/components/build_steps.cljs
+++ src-cljs/frontend/components/build_steps.cljs
@@ -52,6 +52,9 @@
          [:span {:dangerouslySetInnerHTML
                  #js {"__html" trailing-out}}])))))
 
+(defn scroll-to-bottom [element]
+  (.scrollIntoView element false))
+
 (defn action [action owner {:keys [uses-parallelism?] :as opts}]
   (reify
     om/IRender
@@ -108,8 +111,7 @@
                    [:div.action-log-messages
                     (common/messages (:messages action))
                     [:i.click-to-scroll.fa.fa-arrow-circle-o-down.pull-right
-                     {:on-click #(let [target (.-parentNode (.-currentTarget %))]
-                                   (.scrollIntoView target false))}]
+                     {:on-click #(scroll-to-bottom (.-parentNode (.-currentTarget %)))}]
 
                     (when (:bash_command action)
                       [:span
@@ -298,11 +300,7 @@
                    [:div.action-log-messages
                     (common/messages (:messages action))
                     [:i.click-to-scroll.fa.fa-arrow-circle-o-down.pull-right
-                     {:on-click #(let [node (om/get-node owner)
-                                       body (.-body js/document)]
-                                   (set! (.-scrollTop body) (- (+ (.-y (goog.style/getRelativePosition node body))
-                                                                  (.-height (goog.style/getSize node)))
-                                                               (.-height (goog.dom/getViewportSize)))))}]
+                     {:on-click #(scroll-to-bottom (.-parentNode (.-currentTarget %)))}]
                     (when (:bash_command action)
                       [:span
                        (when (:exit_code action)
