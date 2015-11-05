e2c2691aefd804d18ac050a9b264e4b27a18bc77
Travis Vachon <travis.vachon@gmail.com>
2015-11-04 16:51:15 -0800

    If launchdarkly doesn't exist, all features all off
    Easiest fallback for now - we should probably re-integrate query-string
    feature flags soon

--- src-cljs/frontend/utils/launchdarkly.cljs
+++ src-cljs/frontend/utils/launchdarkly.cljs
@@ -2,9 +2,11 @@
 
 (defn feature-on?
   ([feature-name default]
-   (.toggle js/ldclient feature-name default))
+   (when (and js/window.ldclient js/ldclient.toggle)
+    (.toggle js/ldclient feature-name default)))
   ([feature-name]
    (feature-on? feature-name false)))
 
 (defn identify [user]
-  (.identify js/ldclient (clj->js (merge {:key (aget js/ldUser "key")} user))))
+  (when (and js/window.ldclient js/ldclient.identify)
+   (.identify js/ldclient (clj->js (merge {:key (aget js/ldUser "key")} user)))))
