b9e692c50bfb44690987fbafe0f6c890d278e963
Justin Cowperthwaite <justinc474@gmail.com>
2015-11-24 15:01:25 -0800

    added tests for enterprise

--- test-cljs/frontend/models/test_project.cljs
+++ test-cljs/frontend/models/test_project.cljs
@@ -1,6 +1,7 @@
 (ns frontend.models.test-project
   (:require [frontend.models.project :as project]
-            [frontend.test-utils :as test-utils])
+            [frontend.test-utils :as test-utils]
+            [frontend.config :as config])
   (:require-macros [cemerick.cljs.test :refer [is deftest with-test run-tests testing test-var]]))
 
 (def oss-project
@@ -16,12 +17,18 @@
   (is (= "org/repo" (project/project-name {:vcs_url "https://ghe.github.com/org/repo"}))))
 
 (deftest test-show-build-timing-works
-  (is (= (project/show-build-timing? oss-project (test-utils/example-plan :free)) true))
-  (is (= (project/show-build-timing? private-project (test-utils/example-plan :free)) false))
-  (is (= (project/show-build-timing? private-project (test-utils/example-plan :paid)) true)))
+  (with-redefs [config/enterprise? (constantly false)]
+    (is (= (project/show-build-timing? oss-project (test-utils/example-plan :free)) true))
+    (is (= (project/show-build-timing? private-project (test-utils/example-plan :free)) false))
+    (is (= (project/show-build-timing? private-project (test-utils/example-plan :paid)) true)))
+  (with-redefs [config/enterprise? (constantly true)]
+    (is (= (project/show-build-timing? private-project (test-utils/example-plan :free)) true))))
 
 (deftest test-add-show-insights-works
-  (is (= (:show-insights? (project/add-show-insights? oss-project (test-utils/example-user-plans-free)) true)))
-  (is (= (:show-insights? (project/add-show-insights? private-project (test-utils/example-user-plans-free)) false)))
-  (is (= (:show-insights? (project/add-show-insights? private-project (test-utils/example-user-plans-paid)) true)))
-  (is (= (:show-insights? (project/add-show-insights? private-project (test-utils/example-user-plans-piggieback))) true)))
+   (with-redefs [config/enterprise? (constantly false)]
+    (is (= (:show-insights? (project/add-show-insights? oss-project test-utils/example-user-plans-free) true)))
+    (is (= (:show-insights? (project/add-show-insights? private-project test-utils/example-user-plans-free) false)))
+    (is (= (:show-insights? (project/add-show-insights? private-project test-utils/example-user-plans-paid) true)))
+    (is (= (:show-insights? (project/add-show-insights? private-project test-utils/example-user-plans-piggieback)) true))) 
+  (with-redefs [config/enterprise? (constantly true)]
+    (is (= (:show-insights? (project/add-show-insights? private-project test-utils/example-user-plans-free) true)))))
