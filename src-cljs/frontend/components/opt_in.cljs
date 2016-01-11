077cc3fd73a26c18875b69f2cefe5f2ec16295b6
Ian Duncan <ian@iankduncan.com>
2016-01-11 17:23:08 +0900

    Remove opt-in / opt-out banners

--- src-cljs/frontend/components/opt_in.cljs
+++ src-cljs/frontend/components/opt_in.cljs
@@ -7,33 +7,6 @@
             [frontend.components.forms :refer [managed-button]])
   (:require-macros [frontend.utils :refer [html]]))
 
-(defn ui-v2-opt-in-banner [{} owner]
-  (reify
-    om/IDisplayName (display-name [_] "UI V2 Opt In")
-    om/IRender
-    (render [_]
-      (html
-       [:div.ui-v2-opt-in {}
-        [:div.ui-v2-opt-in-wrapper
-         [:div
-          [:span "CircleCI is getting a new look. "]]
-         [:div.opt-in-right
-          [:button {:on-click #(raise! owner [:try-ui-v2-clicked])} "Try our new look"]]]]))))
-
-(defn ui-v2-opt-out-ui [{} owner]
-  (reify
-    om/IDisplayName (display-name [_] "UI V2 Opt In")
-    om/IRender
-    (render [_]
-      (html
-       [:div.ui-v2-opt-out {}
-        (managed-button [:button {:on-click #(raise! owner [:disable-ui-v2-clicked])} "Back to old look"])
-        [:a {:href "mailto:beta@circleci.com?subject=New Look"
-             :target "_blank"
-             :on-click #(raise! owner [:ui-v2-beta-feedback])}
-         "Beta Feedback"]
-        ]))))
-
 (defn ios-reminder-banner []
   (reify
     om/IDisplayName (display-name [_] "iOS Beta End Message")
