d08ad4f59af88eb924c752f2e69de05fab4ee537
Peter Jaros <peter.a.jaros@gmail.com>
2016-01-12 11:55:10 -0500

    Switch off our custom Karma adapter

--- karma.dev.conf.js
+++ karma.dev.conf.js
@@ -1,71 +1,40 @@
-// Karma configuration
-
 var path = require('path');
 
 module.exports = function(config) {
-  
-  var DEV_OUT = 'resources/public/cljs/out';
+  var root = 'resources/public/cljs/out';
 
   config.set({
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [ 'cljsTest'],
+    frameworks: ['cljs-test'],
 
-    // list of files / patterns to load in the browser
     files: [
+      // Provide a stub `window.renderContext`
       'test-js/test-render-context.js',
+
       'resources/public/assets/js/om-dev*.js',
-      path.join(DEV_OUT, 'goog/base.js'),
-      path.join(DEV_OUT, 'frontend-dev.js'),
+
+      path.join(root, 'goog/base.js'),
+
+
+      path.join(root, 'frontend-dev.js'),
+
+      // Require the actual tests.
       'test-js/require-karma.js',
-      { pattern: path.join(DEV_OUT, '**/*.js'), included: false},
-      { pattern: path.join(DEV_OUT, '**/*.cljs'), included: false}
+
+      // Serve the app. Use `included: false` so that Karma itself doesn't load
+      // these files in the browser; we leave that to `goog.require()` and
+      // namespace dependency resolution.
+      { pattern: path.join(root, '**/*.js'), included: false},
+      { pattern: path.join(root, '**/*.js.map'), included: false},
+      { pattern: path.join(root, '**/*.cljs'), included: false}
     ],
 
-
-    // list of files to exclude
-    exclude: [
-      
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-	'frontend-dev.js': ['sourcemap']
+    client: {
+      // main function
+      args: ['circle.karma.run_tests_for_karma']
     },
 
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    reporters: ['progress']
   });
 };
