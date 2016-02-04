a2c5b605cb02737b73bf5fdf239648270aa654b8
Le Wang <l26wang@gmail.com>
2016-02-04 15:48:00 +0800

    use V1 routes for GH objects

--- src-cljs/frontend/api.cljs
+++ src-cljs/frontend/api.cljs
@@ -74,9 +74,15 @@
                                   limit builds-per-page}}]
   (let [url (cond admin "/api/v1/admin/recent-builds"
                   deployments "/api/v1/admin/deployments"
-                  branch (gstring/format "/api/dangerzone/project/%s/%s/%s/tree/%s" (routes/->lengthen-vcs vcs_type) org repo branch)
-                  repo (gstring/format "/api/dangerzone/project/%s/%s/%s" (routes/->lengthen-vcs vcs_type) org repo)
-                  org (gstring/format "/api/v1/organization/%s" org)
+                  branch (case vcs_type
+                           "github" (gstring/format "/api/v1/project/%s/%s/tree/%s" org repo branch)
+                           "bitbucket" (gstring/format "/api/dangerzone/project/%s/%s/%s/tree/%s" vcs_type org repo branch))
+                  repo (case vcs_type
+                         "github" (gstring/format "/api/v1/project/%s/%s" org repo)
+                         "bitbucket" (gstring/format "/api/dangerzone/project/%s/%s/%s" vcs_type org repo))
+                  org (case vcs_type
+                        "github" (gstring/format "/api/v1/organization/%s" org)
+                        "bitbucket" (gstring/format "/api/dangerzone/organization/%s/%s" vcs_type org))
                   :else "/api/v1/recent-builds")]
     (str url "?" (sec/encode-query-params (merge {:shallow true
                                                   :offset offset
