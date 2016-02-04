2428c83d1264c0887bd37da700010b3bbaf5b0f6
Marc O'Morain <marc@circleci.com>
2016-02-04 13:37:16 +0000

    Allow Chrome Canary to be installed with Homebrew.

--- script/devtools.sh
+++ script/devtools.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -ex
 
-/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary \
+if [ -a "/opt/homebrew-cask/Caskroom/google-chrome-canary/latest" ]; then
+  CHROME_PATH="/opt/homebrew-cask/Caskroom/google-chrome-canary/latest"
+else
+  CHROME_PATH="/Applications"
+fi
+
+$CHROME_PATH/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary \
   --remote-debugging-port=9222 \
   --no-first-run \
   https://prod.circlehost:4443 &
