ea2814ce077dc2e173ec5b5bb90fc03b6ec59ca1
Cayenne Geis <cayenne@circleci.com>
2016-02-12 09:03:29 -0500

    Warning about race condition

--- src-cljs/frontend/components/project_settings.cljs
+++ src-cljs/frontend/components/project_settings.cljs
@@ -280,7 +280,7 @@
                               :result-path
                               (conj state/project-data-path :build-cache-clear))))
            [:p "CircleCI saves a copy of your dependencies to prevent downloading them all on each build."]
-           [:p [:b "NOTE: "] "Clearing your dependency cache will cause the next build to completely recreate dependencies. In some cases this can add considerable time to that build."]
+           [:p [:b "NOTE: "] "Clearing your dependency cache will cause the next build to completely recreate dependencies. In some cases this can add considerable time to that build.  Also, you may need to ensure that you have no running builds when using this to prevent the old cache from being resaved."]
            [:hr]
            (clear-cache-button "build" project-data owner)]]
          [:div.card.detailed
@@ -292,7 +292,7 @@
                               :result-path
                               (conj state/project-data-path :source-cache-clear))))
            [:p "CircleCI saves a copy of your source code on our system and pulls only changes since the last build on each branch."]
-           [:p [:b "NOTE: "] "Clearing your source cache will cause the next build to download a fresh copy of your source. In some cases this can add considerable time to that build.  Doing this too frequently or when you have a large number of high parallelism builds also carries some risk of becoming rate-limited by github, which will prevent your builds from being able to download source until the rate-limit expires."]
+           [:p [:b "NOTE: "] "Clearing your source cache will cause the next build to download a fresh copy of your source. In some cases this can add considerable time to that build.  Doing this too frequently or when you have a large number of high parallelism builds also carries some risk of becoming rate-limited by github, which will prevent your builds from being able to download source until the rate-limit expires.  Also, you may need to ensure that you have no running builds when using this to prevent the old cache from being resaved."]
            [:hr]
            (clear-cache-button "source" project-data owner)]]]))))
 
