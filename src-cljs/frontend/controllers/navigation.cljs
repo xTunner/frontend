ed22a3f77f8f1d4b64f814e05746270c18b031d0
Frank Wang <frank.wangtao@gmail.com>
2016-02-24 13:40:04 -0500

    Add vcs_type to project insights data fetch

--- src-cljs/frontend/controllers/navigation.cljs
+++ src-cljs/frontend/controllers/navigation.cljs
@@ -289,7 +289,9 @@
 (defmethod post-navigated-to! :project-insights
   [history-imp navigation-point args previous-state current-state]
   (let [api-ch (get-in current-state [:comms :api])]
-    (api/get-projects api-ch :then #(api/get-projects-builds [(select-keys args [:org :repo :branch])] 1000 api-ch))
+    (api/get-projects api-ch :then (fn []
+                                     (let [build-key (api/project-build-key args)]
+                                       (api/get-projects-builds [build-key] 1000 api-ch))))
     (api/get-user-plans api-ch))
   (set-page-title! "Insights"))
 
