0a944f71b1bb1a6fc2037a2ef3637bd10bbe4898
Le Wang <l26wang@gmail.com>
2016-02-29 16:30:46 -0500

    save build-timing when refresh triggered
    - refactor for readability

--- src-cljs/frontend/controllers/api.cljs
+++ src-cljs/frontend/controllers/api.cljs
@@ -104,13 +104,19 @@
 (defmethod api-event [:projects :success]
   [target message status {:keys [resp]} {:keys [navigation-point] :as current-state}]
   (let [new-projects (map (fn [project] (update project :scopes #(set (map keyword %)))) resp)
-        old-projects-by-build-key (group-by api/project-build-key (get-in current-state state/projects-path))
+        old-projects-lookup (into {} (for [old-p (get-in current-state state/projects-path)]
+                                       [(-> old-p
+                                            api/project-build-key
+                                            (dissoc :branch))
+                                        old-p]))
         processed-new-projects
-        (map (fn [{:keys [default_branch] :as project}]
-               (let [matching-old-project (first (get old-projects-by-build-key (api/project-build-key project)))]
-                 (assoc project
-                        :recent-builds (:recent-builds matching-old-project))))
-             new-projects)]
+        (for [new-project new-projects
+              :let [matching-project (or (old-projects-lookup (-> new-project
+                                                                  api/project-build-key
+                                                                  (dissoc :branch)))
+                                         {})]]
+          (merge new-project
+                 (select-keys matching-project [:recent-builds :build-timing])))]
     (assoc-in current-state state/projects-path processed-new-projects)))
 
 (defmethod post-api-event! [:projects :success]
