4ddf0d612a5565f37606b7628e63eee24ae0fc40
EdwardReed <edwardareed@gmail.com>
2016-03-10 11:24:39 -0800

    Add build-retry api-path util

--- src-cljs/frontend/api/path.cljs
+++ src-cljs/frontend/api/path.cljs
@@ -1,6 +1,7 @@
 (ns frontend.api.path
   (:require [goog.string :as gstring]))
 
+; settings
 (defn settings-path [project]
   (case (:vcs_type project)
     "bitbucket" (gstring/format
@@ -18,3 +19,12 @@
     "github" (gstring/format
                "/api/v1/project/%s/%s/plan"
                (:org project) (:repo project))))
+; builds
+(defn build-retry [{:keys [vcs-type org-name repo-name build-num] :as project}]
+  (case vcs-type
+    "bitbucket" (gstring/format
+                  "/api/dangerzone/project/%s/%s/%s/%s/retry"
+                  vcs-type org-name repo-name build-num)
+    "github" (gstring/format
+               "/api/v1/project/%s/%s/%s/retry"
+               org-name repo-name build-num)))
