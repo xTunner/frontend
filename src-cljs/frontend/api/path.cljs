5811285d052cb861cc5204916bb7ef1f09e1a9b4
EdwardReed <edwardareed@gmail.com>
2016-03-14 12:42:50 -0700

    Refactor api path util
      * Add keys to function params
      * Rename functions

--- src-cljs/frontend/api/path.cljs
+++ src-cljs/frontend/api/path.cljs
@@ -1,26 +1,25 @@
 (ns frontend.api.path
   (:require [goog.string :as gstring]))
 
-; settings
-(defn settings-path [project]
-  (case (:vcs_type project)
+(defn project-settings [vcs-type org repo]
+  (case vcs-type
     "bitbucket" (gstring/format
                   "/api/dangerzone/project/%s/%s/%s/settings"
-                  (:vcs_type project) (:org project) (:repo project))
+                  vcs-type org repo)
     "github" (gstring/format
                "/api/v1/project/%s/%s/settings"
-               (:org project) (:repo project))))
+               org repo)))
 
-(defn settings-plan [project]
-  (case (:vcs_type project)
+(defn project-plan [vcs-type org repo]
+  (case vcs-type
     "bitbucket" (gstring/format
                   "/api/dangerzone/project/%s/%s/%s/plan"
-                  (:vcs_type project) (:org project) (:repo project))
+                  vcs-type org repo)
     "github" (gstring/format
                "/api/v1/project/%s/%s/plan"
-               (:org project) (:repo project))))
-; builds
-(defn build-retry [{:keys [vcs-type org-name repo-name build-num] :as project}]
+               org repo)))
+
+(defn build-retry [vcs-type org-name repo-name build-num]
   (case vcs-type
     "bitbucket" (gstring/format
                   "/api/dangerzone/project/%s/%s/%s/%s/retry"
