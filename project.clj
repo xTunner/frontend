2425fe55a4052f4c2725f3d98b785b44e1287e26
Justin Cowperthwaite <justinc474@gmail.com>
2016-03-16 15:03:34 -0700

    added tests for the controls/button-ajax event firing

--- project.clj
+++ project.clj
@@ -36,6 +36,7 @@
                  ;; Frontend tests
                  [com.cemerick/clojurescript.test "0.3.0"]
                  [org.clojure/tools.reader "0.9.2"]
+                 [circleci/bond "0.2.7"]
 
                  ;; For Dirac DevTools
                  [environ "1.0.1"]]
