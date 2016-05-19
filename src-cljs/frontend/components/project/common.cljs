75d87d988aa5f9397091d7ddb59f46d0eff3b9b4
Justin Cowperthwaite <justinc474@gmail.com>
2016-05-19 16:19:06 -0700

    updated old trial banners to fit our new style guide style

--- src-cljs/frontend/components/project/common.cljs
+++ src-cljs/frontend/components/project/common.cljs
@@ -73,10 +73,9 @@
   (html
     [:div.alert {:class "alert-success"}
        (list (gstring/format "This project is covered by %s's trial of %s containers which expires in %s. "
-                             org-name (plan-model/linux-containers plan) (pluralize days "day"))
+                             org-name (plan-model/trial-containers plan) (pluralize days "day"))
              [:a.pay-now-plain-text {:href plan-path} "Update your plan"]
-             " before the trial expires to continue using these containers."
-             )]))
+             " before the trial expires to continue using these containers.")]))
 
 (defn trial-notice [data owner]
   (reify
