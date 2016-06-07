57fe3edd1d74ae1f9d72fdebb66c2c4a0d378499
Mahmood Ali <mahmood@circleci.com>
2016-06-07 12:41:26 -0400

    Always use API to start backend build
    Avoids creating zfe/ branches unnecesarily and avoids an edge case where
    HEAD circleci/circle build has a skip build indicator

--- script/deploy.sh
+++ script/deploy.sh
@@ -32,24 +32,22 @@ git ls-remote --heads $backend_repo > $heads_file
 # backend_branch may be overridden by the next block
 backend_branch=$CIRCLE_BRANCH
 
+backend_sha=""
 if ! grep -e "refs/heads/${CIRCLE_BRANCH}$" $heads_file ; then
-  # Create a new branch if one doesn't exist
-  backend_dir=$HOME/checkouts/circle
-  git clone $backend_repo $backend_dir
-  # the z is so that it gets pushed to the end of the branch list
-  backend_branch="zfe/$CIRCLE_BRANCH"
-  git -C $backend_dir branch $backend_branch origin/production
-  git -C $backend_dir push origin $backend_branch:$backend_branch
+  backend_branch="zfe%2F$CIRCLE_BRANCH"
+  backend_sha=$(grep -e "refs/heads/production" $heads_file | awk '{print $1}')
 else
-  # Trigger a backend build of this sha1.
-  circle_api=https://circleci.com/api/v1
-  tree_url=$circle_api/project/circleci/circle/tree/$backend_branch
-  http_status=$(curl -o /dev/null \
-                     --silent \
-                     --write-out '%{http_code}\n' \
-                     --header "Content-Type: application/json" \
-                     --data "{\"build_parameters\": {\"FRONTEND_DEPLOY_SHA1\": \"$CIRCLE_SHA1\"}}" \
-                     --request POST \
-                     -L $tree_url?circle-token=$BACKEND_CIRCLE_TOKEN)
-  [[ 201 = $http_status ]]
+  backend_sha=$(grep -e "refs/heads/${CIRCLE_BRANCH}$" | awk '{print $1}')
 fi
+
+# Trigger a backend build of this sha1.
+circle_api=https://circleci.com/api/v1
+tree_url=$circle_api/project/circleci/circle/tree/$backend_branch
+http_status=$(curl -o /dev/null \
+                   --silent \
+                   --write-out '%{http_code}\n' \
+                   --header "Content-Type: application/json" \
+                   --data "{\"revision\": \"$backend_sha\", \"build_parameters\": {\"FRONTEND_DEPLOY_SHA1\": \"$CIRCLE_SHA1\"}}" \
+                   --request POST \
+                   -L $tree_url?circle-token=$BACKEND_CIRCLE_TOKEN)
+[[ 201 = $http_status ]]
