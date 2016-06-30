7f57a4909ecd46454ab06e22bcef64e4c79c158e
Jared Volpe <plainspace@gmail.com>
2016-06-30 12:50:41 -0700

    Comment out side nav

--- src-cljs/frontend/components/app.cljs
+++ src-cljs/frontend/components/app.cljs
@@ -122,7 +122,7 @@
                         {:opts {:keymap keymap
                                 :error-ch (get-in app [:comms :errors])}})
               (when (and inner? logged-in?)
-                (om/build aside/aside-nav (dissoc app-without-container-data :current-build-data)))
+                #_(om/build aside/aside-nav (dissoc app-without-container-data :current-build-data)))
 
               (om/build page app)])))))))
 
