a55aa0cfb0b51a396d66243669072ef220e23687
Justin Cowperthwaite <JustinC474@users.noreply.github.com>
2016-07-01 15:09:23 -0700

    Don't request all project settings on the build page (#1254)
    * Call the settings endpoint less.
    
    Seems like we don't actually use the results in a couple cases.
    
    Also remove an unused wrapper for that endpoint.
    
    * Call the settings endpoint less.
    
    Seems like we don't actually use the results in a couple cases.
    
    Also remove an unused wrapper for that endpoint.
    
    * changed the build page to hit the new /:project/info endpoint
    
    before we were hitting the /:project/settings endpoint which returns
    everything associated with this project. This somewhat makes sense for
    the project-settings page (even though still not really since no page
    shows everything), but makes 0 sense for pages like the build page
    that only need the basic project and the user's scopes.
    
    * pointed the build page to the new paths/project-info endpoint
    
    * Switched :dashboard to also call project-info not project-settings

--- src-cljs/frontend/controllers/navigation.cljs
+++ src-cljs/frontend/controllers/navigation.cljs
@@ -112,8 +112,7 @@
                        api-ch
                        :context {:project-name (str (:org args) "/" (:repo args))})
             (when (:read-settings scopes)
-              (ajax/ajax :get
-                         (api-path/project-settings (:vcs_type args) (:org args) (:repo args))
+              (ajax/ajax :get (api-path/project-info (:vcs_type args) (:org args) (:repo args))
                          :project-settings
                          api-ch
                          :context {:project-name (str (:org args) "/" (:repo args))})
@@ -225,7 +224,7 @@
           (when (and (not (get-in current-state state/project-path))
                      (:repo args) (:read-settings scopes))
             (ajax/ajax :get
-                       (api-path/project-settings vcs-type org repo)
+                       (api-path/project-info vcs-type org repo)
                        :project-settings
                        api-ch
                        :context {:project-name project-name
