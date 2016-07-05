2035995b742dc5e3712b78040ef15e0e70b10a21
Le Wang <l26wang@gmail.com>
2016-07-05 15:23:59 -0400

    add org change tracking to /projects

--- src-cljs/frontend/analytics/core.cljs
+++ src-cljs/frontend/analytics/core.cljs
@@ -56,7 +56,8 @@
     :teammates-invited
     :trial-offer-banner-impression
     :update-parallelism-clicked
-    :update-plan-clicked})
+    :update-plan-clicked
+    :org-clicked})
 
 (def supported-controller-events
   ;; TODO: All these events should be server side
