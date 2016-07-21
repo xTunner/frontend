1608c075b6d330ea6426f487d5ca33611575aabe
Andres Cuervo <andrescuervor@gmail.com>
2016-07-21 11:55:16 -0700

    Make working top bar UI components

--- src-cljs/frontend/components/templates/main.cljs
+++ src-cljs/frontend/components/templates/main.cljs
@@ -7,6 +7,7 @@
             [frontend.config :as config]
             [frontend.models.feature :as feature]
             [frontend.state :as state]
+            [frontend.utils.github :as gh-utils]
             [frontend.utils.seq :refer [dissoc-in]]
             [om.core :as om :include-macros true]
             cljs.pprint
@@ -18,54 +19,104 @@
 
   app            - The entire app state.
   main-content   - A component which forms the main content of the page, which
-                   is everything below the header.
+  is everything below the header.
   header-actions - A component which will be placed on the right in the header.
-                   This is used for page-wide actions."
+  This is used for page-wide actions."
   [{:keys [app main-content header-actions]} owner]
   (reify
     om/IRender
     (render [_]
       (html
-       (let [inner? (get-in app state/inner?-path)
-             logged-in? (get-in app state/user-path)
-             show-inspector? (get-in app state/show-inspector-path)
-             show-footer? (not= :signup (:navigation-point app))
-             ;; simple optimzation for real-time updates when the build is running
-             app-without-container-data (dissoc-in app state/container-data-path)]
-         [:main.app-main
-          [:div.top-bar
-           [:div.bar
-            [:ul.header-nav.left
-              [:li.header-nav-item [:a.header-nav-link "Projects"]]
-              [:li.header-nav-item [:a.header-nav-link "Builds"]]
-              [:li.header-nav-item [:a.header-nav-link "Insights"]]]
-            [:a.logo
-              [:div.logomark
-               (common/ico :logo)]]
-            [:ul.header-nav.right
-              [:li.header-nav-item [:a.header-nav-link "Changelog"]]
-              [:li.header-nav-item [:a.header-nav-link "Documentation"]]
-              [:li.header-nav-item.border-right [:a.header-nav-link "Support"]]
-              [:li.header-nav-item.dropdown [:a.header-nav-link.name.tooltipped
-                [:img.gravatar {:src "https://avatars.githubusercontent.com/u/490421?s=60&amp;v=3"}]
-                [:i.material-icons "keyboard_arrow_down"]]]
-              ]]]
-          (when show-inspector?
-            ;; TODO inspector still needs lots of work. It's slow and it defaults to
-            ;;     expanding all datastructures.
-            (om/build inspector/inspector app))
+        (let [inner? (get-in app state/inner?-path)
+              logged-in? (get-in app state/user-path)
+              show-inspector? (get-in app state/show-inspector-path)
+              show-footer? (not= :signup (:navigation-point app))
+              ;; simple optimzation for real-time updates when the build is running
+              app-without-container-data (dissoc-in app state/container-data-path)
+              user (get-in app state/user-path)
+              avatar-url (gh-utils/make-avatar-url user :size 60)]
+          [:main.app-main
+           ;; TODO -ac Make this into top-bar.cljs
+           [:div.top-bar
+            [:div.bar
+             [:ul.header-nav.left
+              [:a.logo
+               [:div.logomark
+                (common/ico :logo)]]
+              ]
+             [:ul.header-nav.right
+              ;; TODO -ac Hmmm, v2, include the icons? [:i.material-icons "receipt"]
+              [:li.dropdown.header-nav-item
+               [:a.dropbtn.header-nav-link (common/contact-support-a-info owner) "Support"]
+               [:div.dropdown-content
+                [:a {:href "/docs/"}
+                 "Documentation (Self help?)"]
+                [:a {:href "#----idk?"}
+                 "Community Help (Uh, Diqus link?)"]
+                [:a {:href "#-----idk"}
+                 "Engineer Help (Gray this out if not paying??? What link?)"]]]
 
-          (om/build header/header {:app app-without-container-data
-                                   :actions header-actions})
+              [:li.dropdown.header-nav-item
+               [:a.dropbtn.header-nav-link "What's New"]
+               [:div.dropdown-content
+                (when-not (config/enterprise?)
+                  [:a {:href "/changelog/"}
+                   "Changelog"])
+                [:a {:href "https://discuss.circleci.com/c/announcements"}
+                 "Infrastructure Announcements"]]]
 
-          [:div.app-dominant
-           (when (and inner? logged-in?)
-             (om/build aside/aside (dissoc app-without-container-data :current-build-data)))
+              [:li.dropdown.header-nav-item
+               [:a.dropbtn.header-nav-link
+                [:img.gravatar {:src avatar-url}]
+                ;; TODO -ac Ask Jared if this should be a hover or press?
+                [:i.material-icons "keyboard_arrow_down"]]
+               [:div.dropdown-content
+                [:a {:href "/logout/"}
+                 "Logout"]]]]]]
+           ;; TODO -ac REMOVE
+           ;; [:div.top-bar
+           ;;      [:div.flex-wrapper
+           ;;       [:span.logomark
+           ;;       {:style {:height "70px"
+           ;;                :width "35px"
+           ;;                :fill "black"}}
+           ;;       (common/ico :logo)]
+           ;;        [:span.dropdown
+           ;;         ;; [:buttom.dropbtn "(What's new?) | "]
+           ;;         [:span.dropbtn "(What's new?) | "]
+           ;;         [:div.dropdown-content
+           ;;          [:a {:href "#link-1"}
+           ;;           "Sort of long text?"]
+           ;;          [:a {:href "#link-2"}
+           ;;           "Link 2"]
+           ;;          [:a {:href "#link-3"}
+           ;;           "Link 3"]]]
+           ;;        [:div.dropdown
+           ;;         [:span.dropbtn "(Support) | "]
+           ;;         [:div.dropdown-content
+           ;;          [:a {:href "#link-1"}
+           ;;           "Link One"]
+           ;;          [:a {:href "#link-2"}
+           ;;           "Link 2"]
+           ;;          [:a {:href "#link-3"}
+           ;;           "Link 3"]]]
+           ;;         [:img.gravatar {:src avatar-url}]]]
+           (when show-inspector?
+             ;; TODO inspector still needs lots of work. It's slow and it defaults to
+             ;;     expanding all datastructures.
+             (om/build inspector/inspector app))
+
+           (om/build header/header {:app app-without-container-data
+                                    :actions header-actions})
+
+           [:div.app-dominant
+            (when (and inner? logged-in?)
+              (om/build aside/aside (dissoc app-without-container-data :current-build-data)))
 
 
-           [:div.main-body
-            main-content
+            [:div.main-body
+             main-content
 
-            (when (and (not inner?) show-footer? (config/footer-enabled?))
-              [:footer.main-foot
-               (footer/footer)])]]])))))
+             (when (and (not inner?) show-footer? (config/footer-enabled?))
+               [:footer.main-foot
+                (footer/footer)])]]])))))
