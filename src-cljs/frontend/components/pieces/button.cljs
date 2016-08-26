7ef5fdea4dc08a7d1a6e711b8b9daa4174de8d47
Frank Wang <frank.wangtao@gmail.com>
2016-08-26 16:47:55 -0400

    Add managed button component

--- src-cljs/frontend/components/pieces/button.cljs
+++ src-cljs/frontend/components/pieces/button.cljs
@@ -1,5 +1,6 @@
 (ns frontend.components.pieces.button
-  (:require [devcards.core :as dc :refer-macros [defcard]])
+  (:require [devcards.core :as dc :refer-macros [defcard]]
+            [frontend.components.forms :as forms])
   (:require-macros [frontend.utils :refer [component html]]))
 
 (defn button
@@ -19,6 +20,38 @@
                :disabled disabled?
                :on-click on-click}
       content])))
+
+(defn managed-button
+  "A standard button.
+
+  :on-click  - A function called when the button is clicked.
+  :primary?  - If true, the button appears as a primary button. (default: false)
+  :disabled? - If true, the button is disabled. (default: false)
+  :size      - The size of the button. One of #{:full :medium}. (default: :full)"
+  [{:keys [primary? size] :or {size :full} :as opts} content]
+  (forms/managed-button [:button (-> (select-keys opts [:data-failed-text
+                                                        :data-success-text
+                                                        :data-loading-text
+                                                        :on-click])
+                                     (assoc :class (remove nil? [(when primary? "primary")
+                                                                 (case size
+                                                                   :full nil
+                                                                   :medium "medium")])
+                                            ;; normally, manually
+                                            ;; adding :data-component
+                                            ;; is not
+                                            ;; recommended. Here, an
+                                            ;; exception is being made
+                                            ;; because `component` is
+                                            ;; not compatible with om
+                                            ;; components as the root,
+                                            ;; and
+                                            ;; `forms/managed-button`
+                                            ;; takes hiccup as an
+                                            ;; argument instead of an
+                                            ;; element
+                                            :data-component `managed-button))
+                         content]))
 
 
 (dc/do
