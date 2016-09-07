5d3446d7a2d89cc32de704fa683991047debdcfc
Jared Volpe <plainspace@gmail.com>
2016-09-07 11:16:30 -0700

    Refactor markup and make responsive/adaptive things work

--- src-cljs/frontend/components/pieces/topbar.cljs
+++ src-cljs/frontend/components/pieces/topbar.cljs
@@ -27,55 +27,63 @@
                     :aria-label "Dashboard"}
        (common/ico :logo)]
       [:div.navs-container
-       [:ul.nav-items
+        [:ul.nav-items.collapsing-nav
+          [:li
+         [:a {:href "/dashboard"
+                   :aria-label "Builds"}
+          [:i.material-icons "storage"] "Builds"]]
+          [:li
+         [:a {:href "/build-insights"
+                   :aria-label "Insights"}
+          [:i.material-icons "assessment"] "Insights"]]
+          [:li
+         [:a {:href "/projects"
+                   :aria-label "Projects"}
+          [:i.material-icons "book"] "Projects"]]
+          [:li
+         [:a {:href "/team"
+                   :aria-label "Teams"}
+          [:i.material-icons "group"] "Teams"]]]
+        [:ul.nav-items.collapsing-nav
+         [:li.dropdown
+          [:button.dropdown-toggle
+           {:data-toggle "dropdown"
+            :aria-haspopup "true"
+            :aria-expanded "false"}
+           "What's New " [:i.material-icons "keyboard_arrow_down"]]
+          [:ul.dropdown-menu.pull-right.animated.slideInDown
+           (when-not (config/enterprise?)
+             [:li [:a (html/open-ext {:href "https://circleci.com/changelog/"}) "Changelog"]])
+           [:li [:a (html/open-ext {:href "https://discuss.circleci.com/c/announcements"}) "Announcements"]]]]
          [:li
-          [:a {:href "/dashboard"
-                    :aria-label "Builds"}
-           [:i.material-icons "storage"] "Builds"]]
+          [:a (html/open-ext {:href "https://circleci.com/docs/"})
+           "Docs"]]
          [:li
-          [:a {:href "/build-insights"
-                    :aria-label "Insights"}
-           [:i.material-icons "assessment"] "Insights"]]
+          [:a (html/open-ext {:href "https://discuss.circleci.com/"})
+           "Discuss"]]
          [:li
-          [:a {:href "/projects"
-                    :aria-label "Projects"}
-           [:i.material-icons "book"] "Projects"]]
-         [:li
-          [:a {:href "/team"
-                    :aria-label "Teams"}
-           [:i.material-icons "group"] "Teams"]]]
-       [:ul.nav-items
-        [:li.dropdown
-         [:button.dropdown-toggle
-          {:data-toggle "dropdown"
-           :aria-haspopup "true"
-           :aria-expanded "false"}
-          "What's New " [:i.material-icons "keyboard_arrow_down"]]
-         [:ul.dropdown-menu.pull-right.animated.slideInDown
-          (when-not (config/enterprise?)
-            [:li [:a (html/open-ext {:href "https://circleci.com/changelog/"}) "Changelog"]])
-          [:li [:a (html/open-ext {:href "https://discuss.circleci.com/c/announcements"}) "Announcements"]]]]
-        [:li
-         [:a (html/open-ext {:href "https://circleci.com/docs/"})
-          "Docs"]]
-        [:li
-         [:a (html/open-ext {:href "https://discuss.circleci.com/"})
-          "Discuss"]]
-        [:li
-         [:a support-info
-          "Support"]]
-        [:li.dropdown.user-menu
-         [:button.dropdown-toggle
-          {:data-toggle "dropdown"
-           :aria-haspopup "true"
-           :aria-expanded "false"}
-          [:img.gravatar {:src (gh-utils/make-avatar-url user :size 60)}]
-          [:i.material-icons "keyboard_arrow_down"]]
-         [:ul.dropdown-menu.pull-right.animated.slideInDown
-          [:li [:a {:href "/logout/"} "Logout"]]
-          [:li [:a {:href "/account"} "User Settings"]]
-          (when (:admin user)
-            [:li [:a {:href "/admin"} "Admin"]])]]]]])))
+          [:a support-info
+           "Support"]]]
+        [:ul.nav-items
+         [:li.dropdown.user-menu
+          [:button.dropdown-toggle
+           {:data-toggle "dropdown"
+            :aria-haspopup "true"
+            :aria-expanded "false"}
+           [:img.gravatar {:src (gh-utils/make-avatar-url user :size 60)}]
+           [:i.material-icons "keyboard_arrow_down"]]
+          [:ul.dropdown-menu.pull-right.animated.slideInDown
+           [:li [:a {:href "/logout/"} "Logout"]]
+           [:li [:a {:href "/account"} "User Settings"]]
+           (when (:admin user)
+             [:li [:a {:href "/admin"} "Admin"]])]]]
+        [:button.navbar-toggler.hidden-sm-up {:type "button"
+                                                      :data-toggle "collapse"
+                                                      :data-target "collapsing-nav"
+                                                      :aria-controls "collapsing-nav"
+                                                      :aria-expanded "false"
+                                                      :aria-label "Toggle navigation"}
+           [:i.material-icons "menu"]]]])))
 
 (dc/do
   (defcard topbar
