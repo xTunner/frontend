7dd00b6863316075a1c59cac54b7020208764297
Tim Reinke <tim.reinke@circleci.com>
2016-11-04 11:17:16 -0700

    Send AB properties on tracking

--- src-cljs/frontend/analytics/core.cljs
+++ src-cljs/frontend/analytics/core.cljs
@@ -134,7 +134,7 @@
 (def BuildEvent
   (analytics-event-schema {:build {s/Keyword s/Any}}))
 
-(defn- add-properties-to-track-from-state [current-state]
+(defn- properties-to-track-from-state [current-state]
   "Get a map of the mutable properties we want to track out of the
   state. Also add a timestamp."
   {:user (get-in current-state state/user-login-path)
@@ -146,9 +146,9 @@
 (defn- supplement-tracking-properties [{:keys [properties current-state]}]
   "Fill in any unsuppplied property values with those supplied
   in the current app state."
-  (-> current-state
-      (add-properties-to-track-from-state)
-      (merge properties)))
+  (merge (properties-to-track-from-state current-state)
+         {:ab-test-treatments (ab/ab-test-treatments)}
+         properties))
 
 (defn- current-subpage
   "Get the subpage for a pageview. If there is a subpage as well as a tab, the subpage
