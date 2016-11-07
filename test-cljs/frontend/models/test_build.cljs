0c81207fcb37d0be11b879ba197af1d276189f65
Anton Panferov <smaant@gmail.com>
2016-11-07 15:58:16 +0300

    CIRCLE-1609 add tests for data acrobatics with containers steps (#1518)
    * CIRCLE-1609 add tests for data acrobatics with containers steps
    * CIRCLE-1609 review fixes

--- test-cljs/frontend/models/test_build.cljs
+++ test-cljs/frontend/models/test_build.cljs
@@ -37,3 +37,42 @@
          "queued"               "Status-Queued"
          "not_running"          "Status-Queued"
          "scheduled"            "Status-Queued")))
+
+(deftest containers-test
+  (testing "should not modify complete containers"
+    (let [build {:parallel 2
+                 :steps [{:actions [{:index 0 :step 0} {:index 1 :step 0}]}
+                         {:actions [{:index 0 :step 1} {:index 1 :step 1}]}]}]
+      
+      (is (= [{:index 0 :actions [{:index 0 :step 0} {:index 0 :step 1}]}
+              {:index 1 :actions [{:index 1 :step 0} {:index 1 :step 1}]}]
+             (build-model/containers build)))))
+  
+  (testing "should add dummy step for containers without some steps"
+    (let [build {:parallel 2
+                 :steps [{:actions [{:index 0 :step 0} {:index 1 :step 0}]}
+                         {:actions [{:index 0 :step 1}]}
+                         {:actions [{:index 0 :step 2} {:index 1 :step 2}]}]}]
+      
+      (is (= [{:index 0 :actions [{:index 0 :step 0} {:index 0 :step 1} {:index 0 :step 2}]}
+              {:index 1 :actions [{:index 1 :step 0} {:index 1 :step 1 :status "running" :filler-action true} {:index 1 :step 2}]}]
+             (build-model/containers build)))))
+
+  (testing "should replicate not parallel steps"
+    (let [build {:parallel 2
+                 :steps [{:actions [{:index 0 :step 0} {:index 1 :step 0}]}
+                         {:actions [{:index 0 :step 1 :parallel false}]}
+                         {:actions [{:index 0 :step 2} {:index 1 :step 2}]}]}]
+      
+      (is (= [{:index 0 :actions [{:index 0 :step 0} {:index 0 :step 1 :parallel false} {:index 0 :step 2}]}
+              {:index 1 :actions [{:index 1 :step 0} {:index 0 :step 1 :parallel false} {:index 1 :step 2}]}]
+             (build-model/containers build)))))
+
+  (testing "should fill skipped steps"
+    (let [build {:parallel 2
+                 :steps [{:actions [{:index 0 :step 0} {:index 1 :step 0}]}
+                         {:actions [{:index 0 :step 2} {:index 1 :step 2}]}]}]
+      
+      (is (= [{:index 0 :actions [{:index 0 :step 0} {:index 0 :step 1 :status "running" :filler-action true} {:index 0 :step 2}]}
+              {:index 1 :actions [{:index 1 :step 0} {:index 1 :step 1 :status "running" :filler-action true} {:index 1 :step 2}]}]
+             (build-model/containers build))))))
