5d7c2c3b9d0b1871a5c38bb399d9396b6f3490da
Peter Jaros <peter.a.jaros@gmail.com>
2016-11-08 12:53:08 -0800

    The action is "Log Out", not "Logout"

--- src-cljs/frontend/components/aside.cljs
+++ src-cljs/frontend/components/aside.cljs
@@ -565,10 +565,10 @@
            (when-not (ld/feature-on? "top-bar-ui-v-1")
             [:a.aside-item.push-to-bottom {:data-placement "right"
                                            :data-trigger "hover"
-                                           :title "Logout"
+                                           :title "Log Out"
                                            :href "/logout"}
               [:i.material-icons "power_settings_new"]
-              [:div.nav-label "Logout"]])])))))
+              [:div.nav-label "Log Out"]])])))))
 
 
 (defn aside [{:keys [app show-aside-menu?]} owner]
