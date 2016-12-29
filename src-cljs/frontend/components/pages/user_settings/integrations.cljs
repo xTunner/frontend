3828674702297a41244fd0a9d4d614c41848a98b
Peter Jaros <peter.a.jaros@gmail.com>
2016-12-29 13:58:43 -0500

    Extract integration card to component

--- src-cljs/frontend/components/pages/user_settings/integrations.cljs
+++ src-cljs/frontend/components/pages/user_settings/integrations.cljs
@@ -1,13 +1,26 @@
 (ns frontend.components.pages.user-settings.integrations
   (:require [frontend.components.pieces.card :as card]
             [frontend.models.user :as user]
+            [frontend.utils.function-query :as fq :include-macros true]
             [om.next :as om-next :refer-macros [defui]])
   (:require-macros [frontend.utils :refer [html]]))
+
+(defn- card
+  {::fq/queries {:identity [:identity/login]}}
+  [name identity]
+  (card/titled {:title name}
+               (html
+                [:div
+                 [:p "Build and deploy your " name " repositories."]
+                 (if identity
+                   [:p "Connected to " (:identity/login identity) "."]
+                   [:p "Not connected."])])))
 
 (defui Subpage
   static om-next/IQuery
   (query [this]
-    [{:app/current-user [{:user/identities [:identity/type :identity/login]}]}])
+    [{:app/current-user [{:user/identities (fq/merge [:identity/type]
+                                                     (fq/get card :identity))}]}])
   Object
   (render [this]
     (let [{[github-identity] "github"
@@ -15,17 +28,5 @@
           (group-by :identity/type
                     (-> (om-next/props this) :app/current-user :user/identities))]
       (card/collection
-       [(card/titled {:title "GitHub"}
-                     (html
-                      [:div
-                       [:p "Build and deploy your GitHub repositories."]
-                       (if github-identity
-                         [:p "Connected to " (:identity/login github-identity) "."]
-                         [:p "Not connected."])]))
-        (card/titled {:title "Bitbucket"}
-                     (html
-                      [:div
-                       [:p "Build and deploy your Bitbucket repositories."]
-                       (if bitbucket-identity
-                         [:p "Connected to " (:identity/login bitbucket-identity) "."]
-                         [:p "Not connected."])]))]))))
+       [(card "GitHub" github-identity)
+        (card "Bitbucket" bitbucket-identity)]))))
