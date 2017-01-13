b885ea1a8736509ba4ec298679c299500b773459
Ian Davis <jungziege@gmail.com>
2017-01-12 18:59:31 -0800

    Remove all "open-to-outer" links
    Except two, which were re-written to work on enteprise

--- src-cljs/frontend/routes.cljs
+++ src-cljs/frontend/routes.cljs
@@ -1,10 +1,11 @@
 (ns frontend.routes
   (:require [clojure.string :as str]
-            [goog.string :as gstring]
             [compassus.core :as compassus]
             [frontend.async :refer [put!]]
             [frontend.config :as config]
+            [frontend.utils.github :as github]
             [frontend.utils.vcs :as vcs]
+            [goog.string :as gstring]
             [secretary.core :as sec :refer-macros [defroute]]
             [om.next :as om-next])
   (:require-macros
@@ -31,10 +32,6 @@
   [app nav-ch navigation-point args]
   (compassus/set-route! app :route/legacy-page)
   (put! nav-ch [navigation-point (assoc args :inner? true)]))
-
-;; TODO: Remove this and everything that uses it. It should be completely obsolete.
-(defn open-to-outer! [nav-ch navigation-point args]
-  (put! nav-ch [navigation-point (assoc args :inner? false)]))
 
 (defn logout! [nav-ch]
   (put! nav-ch [:logout]))
@@ -293,102 +290,25 @@
   (defroute v1-logout "/logout" []
     (logout! nav-ch))
 
-  (defroute v1-mobile "/mobile" {:as params}
-    (open-to-outer! nav-ch :mobile (assoc params
-                                     :_title "Mobile Continuous Integration and Mobile App Testing"
-                                     :_description "Build 5-star mobile apps with Mobile Continuous Integration by automating your build, test, and deployment workflow on iOS and Android. ")))
-
-  (defroute v1-ios "/mobile/ios" {:as params}
-    (open-to-outer! nav-ch :ios (assoc params
-                                  :_title "Apple iOS App Testing"
-                                  :_description "Build 5-star iOS apps by automating your development workflow with Mobile Continuous Integration and Delivery.")))
-
-  (defroute v1-android "/mobile/android" {:as params}
-    (open-to-outer! nav-ch :android (assoc params
-                                      :_title "Android App Testing"
-                                      :_description "Build better Android apps with Mobile Continuous Integration. Get testing today!")))
-
-  (defroute v1-pricing "/pricing" {:as params}
-    (open-to-outer! nav-ch :pricing (assoc params
-                                        :_analytics-page "View Pricing Outer"
-                                        :_title "Pricing and Information"
-                                        :_description "Save time and cost by making your engineering team more efficient. Get started for free and see how many containers and parallelism you need to scale with your team.")))
-
-  (defroute v1-jobs "/jobs" {:as params}
-    (open-to-outer! nav-ch :jobs (assoc params
-                                   :_analytics-page "View jobs"
-                                   :_title "Search for Jobs at CircleCI"
-                                   :_description "Come work with us. Join our amazing team of highly technical engineers and business leaders to help us build great developer tools.")))
-
-  (defroute v1-privacy "/privacy" {:as params}
-    (open-to-outer! nav-ch :privacy (assoc params
-                                      :_analytics-page "View Privacy"
-                                      :_title "Privacy Policy"
-                                      :_description "Read our privacy policy to understand how we collect and use information about you.")))
-
-  (defroute v1-security "/security" {:as params}
-    (open-to-outer! nav-ch :security (assoc params
-                                       :_analytics-page "View Security"
-                                       :_title "Security Policy"
-                                       :_description "Read our security policy and guidelines and see how your data is safe with CircleCI.")))
-
-  (defroute v1-security-hall-of-fame "/security/hall-of-fame" {:as params}
-    (open-to-outer! nav-ch :security-hall-of-fame (assoc params
-                                                    :_title "Security Hall of Fame"
-                                                    :_description "Join our Security Hall of Fame by helping us make our platform more secure."
-                                                    :_analytics-page "View Security Hall of Fame")))
-
-  (defroute v1-enterprise "/enterprise" {:as params}
-    (open-to-outer! nav-ch :enterprise (assoc params
-                                         :_title "Enterprise Continuous Integration and Deployment"
-                                         :_description "Reduce risk with Enterprise Continuous Integration from CircleCI. Integrates seamlessly with Github Enterprise and the rest of your technology stack.")))
-
-  (defroute v1-enterprise-aws "/enterprise/aws" {:as params}
-    (open-to-outer! nav-ch :aws (assoc params
-                                         :_title "CircleCI Enterprise on AWS"
-                                         :_description "Run CircleCI Enterprise on the same AWS infrastructure you use for everything else. Integrates seamlessly with GitHub Enterprise on AWS.")))
-
-  (defroute v1-enterprise-azure "/enterprise/azure" {:as params}
-    (open-to-outer! nav-ch :azure (assoc params
-                                         :_title "CircleCI Enterprise on Azure"
-                                         :_description "Install CircleCI Enterprise in your own Microsoft Azure account. Integrates with GitHub Enterprise on Azure and Active Directory authentication.")))
-
-  (defroute v1-stories "/stories/:story" [story]
-    (open-to-outer! nav-ch :stories {:story (keyword story)}))
-
-  (defroute v1-features "/features" {:as params}
-    (open-to-outer! nav-ch :features (assoc params
-                                       :_title "Continuous Integration Product and Features"
-                                       :_description "Build a better product and let CircleCI handle your testing. CircleCI helps your team improve productivity with faster development, reduced risk, and better code.")))
-
-  (defroute v1-languages "/features/:language" {:as params}
-    (open-to-outer! nav-ch :language-landing params))
-
-  (defroute v1-integrations "/integrations/:integration" [integration]
-    (open-to-outer! nav-ch :integrations {:integration (keyword integration)}))
-
   (defroute v1-root "/" {:as params}
-    (if authenticated?
-      (open-to-inner! app nav-ch :dashboard params)
-      ;; Note: This actually uses the outer styles, but rather than use
-      ;; `open-to-outer!`, it's special-cased in
-      ;; `frontend.components.app/Wrapper`. Meanwhile `open-to-outer!` simply
-      ;; doesn't work anymore, and needs to be cleaned up.
+    (cond
+      (not (config/enterprise?))
+      (set! js/window.location "https://circleci.com/")
+
+      authenticated?
+      (open-to-inner! app nav-ch :dashboard (assoc params :_canonical "/dashboard"))
+
+      :else
       (open-to-inner! app nav-ch :landing (assoc params :_canonical "/"))))
 
   (defroute v1-dashboard "/dashboard" {:as params}
     (open-to-inner! app nav-ch :dashboard params))
 
-  (defroute v1-home "/home" {:as params}
-    (open-to-outer! nav-ch :landing (assoc params :_canonical "/")))
-
-  (defroute v1-press "/press" {:as params}
-    (open-to-outer! nav-ch :press (assoc params
-                                         :_title "Press Releases and Updates"
-                                         :_description "Find the latest CircleCI news and updates here.")))
-
   (defroute v1-signup "/signup" {:as params}
-    (open-to-outer! nav-ch :signup params)))
+    (set! js/window.location
+          (if (config/enterprise?)
+            (github/auth-url)
+            "https://circleci.com/signup"))))
 
 (defn define-routes! [current-user app nav-ch]
   (let [authenticated? (boolean current-user)]
