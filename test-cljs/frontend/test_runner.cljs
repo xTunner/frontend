222b5de8490f7ce845286cf4ae590d2741ff0f40
AlexanderMann <alex@circleci.com>
2017-01-18 11:20:36 -0500

    trying doo-all-tests

--- test-cljs/frontend/test_runner.cljs
+++ test-cljs/frontend/test_runner.cljs
@@ -1,60 +1,7 @@
 (ns frontend.test-runner
-  (:require [cljs.test :refer-macros [use-fixtures]]
-            [clojure.string]
-            [doo.runner :refer-macros [doo-tests]]
-            [frontend.analytics.test-core]
-            [frontend.analytics.test-segment]
-            [frontend.analytics.test-track]
-            [frontend.components.project.test-common]
-            [frontend.components.test-build-head]
-            [frontend.components.test-insights]
-            [frontend.components.test-org-settings]
-            [frontend.components.test-statuspage]
-            [frontend.controllers.test-api]
-            [frontend.controllers.test-controls]
-            [frontend.controllers.test-ws]
-            [frontend.models.test-action]
-            [frontend.models.test-build]
-            [frontend.models.test-feature]
-            [frontend.models.test-plan]
-            [frontend.models.test-project]
-            [frontend.models.test-user]
-            [frontend.test-analytics]
-            [frontend.test-datetime]
-            [frontend.test-elevio]
-            [frontend.test-pusher]
-            [frontend.test-routes]
-            [frontend.utils-test]
-            [frontend.utils.test-build]
-            [frontend.utils.test-function-query]
-            [frontend.utils.test-seq]))
+  (:require [doo.runner :refer-macros [doo-all-tests]]))
 
 (aset js/window "renderContext" "{}")
 (aset js/window "SVGInjector" (fn [node] node))
 
-(doo-tests 'frontend.analytics.test-core
-           'frontend.analytics.test-segment
-           'frontend.analytics.test-track
-           'frontend.components.project.test-common
-           'frontend.components.test-build-head
-           'frontend.components.test-insights
-           'frontend.components.test-org-settings
-           'frontend.components.test-statuspage
-           'frontend.controllers.test-api
-           'frontend.controllers.test-controls
-           'frontend.controllers.test-ws
-           'frontend.models.test-action
-           'frontend.models.test-build
-           'frontend.models.test-feature
-           'frontend.models.test-plan
-           'frontend.models.test-project
-           'frontend.models.test-user
-           'frontend.test-analytics
-           'frontend.test-datetime
-           'frontend.test-elevio
-           'frontend.test-pusher
-           'frontend.test-routes
-           'frontend.utils-test
-           'frontend.utils.test-build
-           'frontend.utils.test-function-query
-           'frontend.utils.test-seq)
+(doo-all-tests)
