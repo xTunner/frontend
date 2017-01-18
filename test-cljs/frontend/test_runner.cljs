84c0ad5ec1c2368afb6499a063c01a0e7deebc3e
AlexanderMann <alex@circleci.com>
2017-01-18 11:47:53 -0500

    using doo-all-tests

--- test-cljs/frontend/test_runner.cljs
+++ test-cljs/frontend/test_runner.cljs
@@ -1,5 +1,33 @@
 (ns frontend.test-runner
-  (:require [doo.runner :refer-macros [doo-all-tests]]))
+  (:require [cljs.test :refer-macros [use-fixtures]]
+            [clojure.string]
+            [doo.runner :refer-macros [doo-all-tests]]
+            [frontend.analytics.test-core]
+            [frontend.analytics.test-segment]
+            [frontend.analytics.test-track]
+            [frontend.components.project.test-common]
+            [frontend.components.test-build-head]
+            [frontend.components.test-insights]
+            [frontend.components.test-org-settings]
+            [frontend.components.test-statuspage]
+            [frontend.controllers.test-api]
+            [frontend.controllers.test-controls]
+            [frontend.controllers.test-ws]
+            [frontend.models.test-action]
+            [frontend.models.test-build]
+            [frontend.models.test-feature]
+            [frontend.models.test-plan]
+            [frontend.models.test-project]
+            [frontend.models.test-user]
+            [frontend.test-analytics]
+            [frontend.test-datetime]
+            [frontend.test-elevio]
+            [frontend.test-pusher]
+            [frontend.test-routes]
+            [frontend.utils-test]
+            [frontend.utils.test-build]
+            [frontend.utils.test-function-query]
+            [frontend.utils.test-seq]))
 
 (aset js/window "renderContext" "{}")
 (aset js/window "SVGInjector" (fn [node] node))
