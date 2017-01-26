791513aa7ff92298eb4dc51d6c4df9d78e0a07ed
An Nguyen <annguy3n@users.noreply.github.com>
2017-01-26 14:58:39 -0800

    [CIRCLE-2649] Update debug messages (#1671)

--- src-cljs/frontend/analytics/amplitude.cljs
+++ src-cljs/frontend/analytics/amplitude.cljs
@@ -1,17 +1,12 @@
 (ns frontend.analytics.amplitude
   (:require [goog.net.cookies :as cookies]
-            [frontend.utils :as utils :include-macros true]))
+            [frontend.utils :as utils :refer-macros [swallow-errors]]))
 
 (def session-cookie-name "amplitude-session-id")
 
 (defn session-id []
-  (utils/swallow-errors
-    (let [sid (js/amplitude.getSessionId)]
-      (when (not sid)
-        (println sid)
-        (println js/amplitude)
-        (println js/window.amplitude))
-      (or sid -1))))
+  (swallow-errors
+    (or (js/amplitude.getSessionId) -1)))
 
 (defn set-session-id-cookie!
   "Cookies the user with their amplitude session-id. Set the cookie
@@ -20,6 +15,10 @@
   Set the opt_maxAge to -1 as it is a session cookie:
     https://google.github.io/closure-library/api/goog.net.Cookies.html"
   []
-  (cookies/set session-cookie-name (session-id) -1 "/")
-  (println "set cookie")
-  (println (cookies/get session-cookie-name)))
+  (let [sid (session-id)]
+    (println "setting cookie with sid" sid)
+    (cookies/set session-cookie-name sid -1 "/")
+    (println "set" (cookies/get session-cookie-name))
+    (when (not (cookies/get session-cookie-name))
+      (println js/amplitude)
+      (println js/window.amplitude))))
