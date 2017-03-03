97e79774cbc24daf8a1998e73dfea92bdecfd0c1
Andres Cuervo <andrescuervor@gmail.com>
2017-03-02 22:37:51 -0500

    Add analytics to the build page annotation

--- src-cljs/frontend/components/pieces/page_header.cljs
+++ src-cljs/frontend/components/pieces/page_header.cljs
@@ -197,7 +197,8 @@
                (popover/popover {:body
                                  (html [:span "This build ran on 2.0. "
                                         [:a {:href "https://discuss.circleci.com/c/circleci-2-0"
-                                             :target "_blank"}
+                                             :target "_blank"
+                                             :on-click #((om/get-shared owner :track-event) {:event-type :beta-build-page-icon-clicked})}
                                          "Learn more"]])
                                  :trigger-mode :hover
                                  :placement :bottom}
