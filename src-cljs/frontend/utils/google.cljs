1b18c7ebf3bc928c4f2e1637912c3db0f9872739
An Nguyen <annguy3n@users.noreply.github.com>
2017-04-21 14:10:00 -0700

    use return-to correctly (#1854)

--- src-cljs/frontend/utils/google.cljs
+++ src-cljs/frontend/utils/google.cljs
@@ -1,16 +1,8 @@
 (ns frontend.utils.google
-  (:require
-   [frontend.utils :as utils]
-   [cemerick.url :refer [url]]))
+  (:require [cemerick.url :refer [url]]))
 
 (defn auth-url []
-  (let [auth-host (aget js/window "renderContext" "auth_host")
-        state (merge {"return-to" (str js/window.location.pathname
-                                       js/window.location.hash)
-                      "token" (utils/oauth-csrf-token)}
-                     (when (not= auth-host js/window.location.host)
-                       {"delegate" (str js/window.location.protocol "//" js/window.location.host)}))
-        state-str (.stringify js/JSON (clj->js state))]
-    (-> (url (str js/window.location.protocol "//" js/window.location.host "/google-login"))
-        (assoc :query {"state" state-str})
-        str)))
+  (-> (url (str js/window.location.protocol "//" js/window.location.host "/google-login"))
+      (assoc :query {"return-to" (str js/window.location.pathname
+                                      js/window.location.hash)})
+      str))
