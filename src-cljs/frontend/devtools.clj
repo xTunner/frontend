fe11f0d98a21c65fb3e7c241572b69320f796f9b
Frank Wang <frank.wangtao@gmail.com>
2017-05-08 14:20:30 -0400

    Remove dirac devtools macro ns

--- src-cljs/frontend/devtools.clj
+++
@@ -1,7 +0,0 @@
-(ns frontend.devtools
-  (:require [environ.core :refer [env]]))
-
-(defmacro require-devtools! []
-  (when (:devtools env)
-    ;; TODO: Replace this with a ClojureScript require function
-    '(js/goog.require "frontend.devtools")))
