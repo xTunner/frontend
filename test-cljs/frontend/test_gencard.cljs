043efa03feeaa1e6420c4e76250683e82449705e
Peter Jaros <peter.a.jaros@gmail.com>
2017-06-20 17:55:42 -0400

    Return nil for infinite morphs

--- test-cljs/frontend/test_gencard.cljs
+++ test-cljs/frontend/test_gencard.cljs
@@ -1,5 +1,5 @@
 (ns frontend.test-gencard
-  (:require [cljs.test :refer-macros [is]]
+  (:require [cljs.test :refer-macros [is testing]]
             [clojure.spec :as s :include-macros true]
             [clojure.test.check.generators :as gen]
             [goog.object :as gobject])
@@ -22,28 +22,17 @@
                        (gobject/getKeys props))))))
 
 (defn morph-data [component spec]
-  (->> (gen/sample-seq (s/gen spec))
-       (take 100)
-       (group-by (comp signature component))
-       (sort-by (comp hash key))
-       vals
-       (map first)))
+  (let [sample-size 100
+        groups (->> (gen/sample-seq (s/gen spec))
+                    (take sample-size)
+                    (group-by (comp signature component)))]
+    (when (> sample-size (count groups))
+      (->> groups
+           (sort-by (comp hash key))
+           vals
+           (map first)))))
 
 
-
-(s/def ::data-for-component
-  (s/keys :req [::type ::description]))
-
-(s/def ::type #{:type-a :type-b})
-
-(s/def ::description (s/and string? seq))
-
-(defn component [props]
-  (html
-   [:div {:class (name (::type props))}
-    [:.description
-     (when (< 2 (count (::description props))) {:class "long"})
-     (::description props)]]))
 
 (deftest signature-test
   (is (= {:type "div"
@@ -55,12 +44,31 @@
                            [:span {:class "baz" :title "qux"}
                             "Some text."]])))))
 
+(s/def ::data-for-component
+  (s/keys :req [::type ::description]))
+
+(s/def ::type #{:type-a :type-b})
+
+(s/def ::description (s/and string? seq))
+
+(defn demo-component [props]
+  (html
+   [:div {:class (name (::type props))}
+    [:.description
+     (when (< 2 (count (::description props))) {:class "long"})
+     (::description props)]]))
+
 (deftest morph-data-test
-  (dotimes [_ 10]
-    (let [data (morph-data component ::data-for-component)]
-      (is (every? (partial s/valid? ::data-for-component) data))
-      (is (= 4 (count data)))
-      (is (= 2 (count (group-by ::type data)))))))
+  (testing "Generates one set of data for each morph"
+    (dotimes [_ 10]
+      (let [data (morph-data demo-component ::data-for-component)]
+        (is (every? (partial s/valid? ::data-for-component) data))
+        (is (= 4 (count data)))
+        (is (= 2 (count (group-by ::type data)))))))
+
+  (testing "Returns nil when there are infinite morphs"
+    (let [faulty-component #(html [:div {:class (::description %)}])]
+      (is (nil? (morph-data faulty-component ::data-for-component))))))
 
 (defcard demo
   (html
@@ -69,5 +77,5 @@
      ".type-a { background-color: blue; }
       .type-b { background-color: green; }
       .long { color: red; }"]
-    (let [data (morph-data component ::data-for-component)]
-      (map component data))]))
+    (let [data (morph-data demo-component ::data-for-component)]
+      (map demo-component data))]))
