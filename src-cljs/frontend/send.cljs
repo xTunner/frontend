3312bc1e879466c273fae84ce3c1cc0e446a567d
Nate Smith <nwjsmith@gmail.com>
2017-06-21 15:58:29 -0400

    Resolve the top-level run, instead of its keys

--- src-cljs/frontend/send.cljs
+++ src-cljs/frontend/send.cljs
@@ -281,42 +281,10 @@
 
    :circleci/run
    (fn [env ast]
-     (resolve/resolve (assoc env :run/id (:run/id (:params ast)))
-                      ast
-                      (chan)))
-
-   :run/job
-   (fn [env ast]
-     (resolve/resolve (assoc env :job/name (:job/name (:params ast)))
-                      ast
-                      (chan)))
-
-   :job/name
-   (fn [env ast]
-     (:job/name env))
-
-   :run/id
-   (fn [env ast]
-     (:run/id env))
-
-   #{:run/name
-     :run/status
-     :run/started-at
-     :run/stopped-at
-     :run/errors
-     :run/jobs
-     :run/trigger-info
-     :run/project}
-   (fn [{:keys [run/id] :as env} asts]
-     (-> ((get-in env [:apis :get-workflow-status]) id)
+     (-> ((get-in env [:apis :get-workflow-status]) (-> ast :params :run/id))
          (p/then
           (fn [response]
-            (-> response
-                adapt-to-run
-                (update :run/errors #(mapv (partial resolve/query (:run/errors asts)) %))
-                (update :run/jobs #(mapv (partial resolve/query (:run/jobs asts)) %))
-                (update :run/trigger-info (partial resolve/query (:run/trigger-info asts)))
-                (update :run/project (partial resolve/query (:run/project asts))))))))})
+            (resolve/query ast (adapt-to-run response))))))})
 
 (defmulti send* key)
 
